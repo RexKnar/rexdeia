3d6f614f2921ed57b8764a6ec1e5600e5380db0d
arunselvakumar <b.arunselvakumar@gmail.com>
2023-06-03 11:05:02 +0530

    Integrated clerk changes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,20 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
+export const metadata = {
+  title: 'Capeo - Simplifying the way you manage your business',
+  description: 'Capeo is a business management platform for small businesses.',
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,14 @@
-import { Button, Header } from "ui";
+import { UserButton } from '@clerk/nextjs';
+import { Button, Header } from 'ui';
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </>
   );
 }
