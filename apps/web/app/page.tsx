b60407850765a35f65cadf84c62eb6894c4feb52
arunselvakumar <b.arunselvakumar@gmail.com>
2023-07-02 20:55:16 +0530

    -- Integrated Menu for the default route.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,8 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../lib/auth";
-import { redirect } from "next/navigation";
-import { NavigationHeader } from "../lib/components/NavigationHeader";
-import { GraduationCap } from "lucide-react";
-
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../lib/auth';
+import { redirect } from 'next/navigation';
+import { NavigationHeader } from '../lib/components/NavigationHeader';
+import { GraduationCap } from 'lucide-react';
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
@@ -12,9 +11,9 @@ export default async function Page() {
   }
 
   return (
-    <header className="border border-color-50 flex items-center px-4 py-2">
+    <header className="border-color-50 flex items-center border px-4 py-2">
       <div className="flex items-center gap-2">
-        <GraduationCap className="h-6 w-6"/>
+        <GraduationCap className="h-6 w-6" />
         <p className="text-bold text-lg">Capeo</p>
       </div>
       <NavigationHeader />
--- apps/web/lib/components/NavigationHeader.tsx
+++ apps/web/lib/components/NavigationHeader.tsx
@@ -1,57 +1,59 @@
-"use client";
+'use client';
 
 import {
-  NavigationMenu, NavigationMenuContent,
+  NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList, NavigationMenuTrigger
-} from "ui";
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from 'ui';
 import { cn } from 'utils';
-import React from "react";
-import { User2Icon } from "lucide-react";
-import Link from "next/link";
+import React from 'react';
+import { User2Icon } from 'lucide-react';
+import Link from 'next/link';
 
 const components: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
+    title: 'Alert Dialog',
+    href: '/docs/primitives/alert-dialog',
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      'A modal dialog that interrupts the user with important content and expects a response.',
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
+    title: 'Hover Card',
+    href: '/docs/primitives/hover-card',
     description:
-      "For sighted users to preview content available behind a link.",
+      'For sighted users to preview content available behind a link.',
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
+    title: 'Progress',
+    href: '/docs/primitives/progress',
     description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
+    title: 'Scroll-area',
+    href: '/docs/primitives/scroll-area',
+    description: 'Visually or semantically separates content.',
   },
   {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
+    title: 'Tabs',
+    href: '/docs/primitives/tabs',
     description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
   },
   {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
+    title: 'Tooltip',
+    href: '/docs/primitives/tooltip',
     description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
   },
 ];
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
@@ -59,21 +61,21 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            'hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors',
             className
           )}
           {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
             {children}
           </p>
         </a>
       </NavigationMenuLink>
     </li>
-  )
-})
-ListItem.displayName = "ListItem"
+  );
+});
+ListItem.displayName = 'ListItem';
 
 export function NavigationHeader() {
   return (
@@ -86,14 +88,14 @@ export function NavigationHeader() {
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
                   <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    className="from-muted/50 to-muted flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
                     <User2Icon className="h-6 w-6" />
                     <div className="mb-2 mt-4 text-lg font-medium">
                       shadcn/ui
                     </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
+                    <p className="text-muted-foreground text-sm leading-tight">
                       Beautifully designed components built with Radix UI and
                       Tailwind CSS.
                     </p>
@@ -130,9 +132,7 @@ export function NavigationHeader() {
         </NavigationMenuItem>
         <NavigationMenuItem>
           <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink>
-              Documentation
-            </NavigationMenuLink>
+            <NavigationMenuLink>Documentation</NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
