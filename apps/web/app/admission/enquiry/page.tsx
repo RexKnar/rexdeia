4aef70e985933c33dc974b6b55b3ca940f81ed73
arunselvakumar <b.arunselvakumar@gmail.com>
2023-09-03 13:00:10 +0530

    Remove unused 'AdmissionForm' and extend 'FormModel' types
    Removed the import and reference to the unused component 'AdmissionForm' in the enquiry page file to clean up code. Expanded 'FormModel' and 'SearchFormModel' types in forms/models.ts to include 'Enquiry' type, making the form more versatile for different types of enquiries and improving database structure.

--- apps/web/app/admission/enquiry/page.tsx
+++ apps/web/app/admission/enquiry/page.tsx
@@ -3,7 +3,6 @@ import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { getOrganisationsByUserId } from '../../api/user/organization/service';
 import { searchForms } from '../../api/forms/service';
-import { AdmissionForm } from '../../../lib/components/admission/AdmissionForm';
 import { EnquiryForm } from '../../../lib/components/enquiry/EnquiryForm';
 
 export default async function Page() {
