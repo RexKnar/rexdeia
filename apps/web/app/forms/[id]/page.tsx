38770342ed822ca51c14783c58202fa8f9eb121f
arunselvakumar <b.arunselvakumar@gmail.com>
2023-09-19 10:46:23 +0530

    Add static branchId to admission form fetch
    Added a static branchId to the getFormByCriteria function in the form page of the web app. This is a temporary change until the function can be updated to fetch the branchId from the user's session or profile. This will allow the form page to correctly fetch the relevant admission forms for the specific branch the user is associated with.

--- apps/web/app/forms/[id]/page.tsx
+++ apps/web/app/forms/[id]/page.tsx
@@ -7,6 +7,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   const admissionForms = await getFormByCriteria({
     type: 'Admission',
     organizationId: params.id,
+    branchId: '5f1f0f1a0b1eaf001c3f2b1e', // TODO: get branchId from user
   });
   const form = admissionForms[0];
   return (
