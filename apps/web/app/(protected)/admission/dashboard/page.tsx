1dd1443abbeca47cb8dc912946c3658e005754e9
PrabhuSaravanan <prabhu.saravanan@icloud.com>
2023-10-04 16:30:27 +0530

    ...Dashboard UI Base Set

--- apps/web/app/(protected)/admission/dashboard/page.tsx
+++ apps/web/app/(protected)/admission/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { Check, CircleDashed, FileInput, X } from 'lucide-react';
+import { Button } from 'ui';
+
 import { getAdmissionList } from '../../../api/admissionlist/service';
 import { AdmissionListModel, columns } from './columns';
 import { DataTable } from './data-table';
@@ -12,12 +15,103 @@ export default async function Page() {
   const data = await getData();
   return (
     <>
-      <section className="w-full p-3">
-        <h1 className="mt-4 text-center text-3xl font-semibold text-primary">
-          Admission List
-        </h1>
-        <div className="container mx-auto py-10">
-          <DataTable columns={columns} data={data} />
+      <section className="h-screen w-full bg-gray-50 p-3">
+        <div className="mx-6 mt-8">
+          <div className="flex justify-between">
+            <div>
+              <span className="text-xs font-medium text-gray-700">Acadx</span>{' '}
+              <span className="text-gray-700">/</span>
+              <span className="text ml-2 text-sm font-semibold text-gray-700">
+                Dashboard
+              </span>
+              <h1 className="mt-2 font-semibold">Dashboard</h1>
+            </div>
+            <div className="relative flex">
+              <Button className="text-white">
+                <FileInput size={16} className="mr-2" />
+                Admission Form
+              </Button>
+            </div>
+          </div>
+
+          <div>
+            <div className="mt-8 grid grid-cols-4 gap-3 sm:grid sm:grid-cols-1 md:grid md:grid-cols-2 lg:grid lg:grid-cols-4">
+              <div className="flex rounded-md bg-white p-3 shadow">
+                <div className="rounded-full bg-blue-50 p-3">
+                  <FileInput className="text-blue-500" />
+                </div>
+                <div className="ml-4">
+                  <span className="text-sm text-gray-700">
+                    Admission Request
+                  </span>
+                  <h1 className="font-semibold">1.5K</h1>
+                </div>
+              </div>
+              <div className="flex rounded-md bg-white p-3 shadow">
+                <div className="rounded-full bg-blue-50 p-3">
+                  <Check className="text-blue-500" />
+                </div>
+                <div className="ml-4">
+                  <span className="text-sm text-gray-700">Short listed</span>
+                  <h1 className="font-semibold">450</h1>
+                </div>
+              </div>
+              <div className="flex rounded-md bg-white p-3 shadow">
+                <div className="rounded-full bg-blue-50 p-3">
+                  <X className="text-blue-500" />
+                </div>
+                <div className="ml-4">
+                  <span className="text-sm text-gray-700">Rejected</span>
+                  <h1 className="font-semibold">3.5</h1>
+                </div>
+              </div>
+              <div className="flex rounded-md bg-white p-3 shadow">
+                <div className="rounded-full bg-blue-50 p-3">
+                  <CircleDashed className="text-blue-500" />
+                </div>
+                <div className="ml-4">
+                  <span className="text-sm text-gray-700">In-progress</span>
+                  <h1 className="font-semibold">3K</h1>
+                </div>
+              </div>
+            </div>
+            <div className="mt-3 gap-3 sm:grid sm:grid-cols-1 md:grid md:grid-cols-2">
+              <div className="rounded-md bg-white p-3 shadow">
+                Generating random paragraphs can be an excellent way for writers
+                to get their creative flow going at the beginning of the day.
+                The writer has no idea what topic the random paragraph will be
+                about when it appears. This forces the writer to use creativity
+                to complete one of three common writing challenges. The writer
+                can use the paragraph as the first one of a short story and
+                build upon it. A second option is to use the random paragraph
+                somewhere in a short story they create. The third option is to
+                have the random paragraph be the ending paragraph in a short
+                story. No matter which of these challenges is undertaken, the
+                writer is forced to use creativity to incorporate the paragraph
+                into their writing
+              </div>
+              <div className="rounded-md bg-white p-3 shadow">
+                Generating random paragraphs can be an excellent way for writers
+                to get their creative flow going at the beginning of the day.
+                The writer has no idea what topic the random paragraph will be
+                about when it appears. This forces the writer to use creativity
+                to complete one of three common writing challenges. The writer
+                can use the paragraph as the first one of a short story and
+                build upon it. A second option is to use the random paragraph
+                somewhere in a short story they create. The third option is to
+                have the random paragraph be the ending paragraph in a short
+                story. No matter which of these challenges is undertaken, the
+                writer is forced to use creativity to incorporate the paragraph
+                into their writing
+              </div>
+            </div>
+          </div>
+          <h1 className="mt-96 text-center text-3xl font-semibold text-primary">
+            Admission List
+          </h1>
+          <div className="mx-auto py-10">
+            <DataTable columns={columns} data={data} />
+          </div>
         </div>
       </section>
     </>
