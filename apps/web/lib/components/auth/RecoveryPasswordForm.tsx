6e3b3aa7b53fbc9473ed2ffb3f46817a006acbcd
Arasu <malarasuk@gmail.com>
2023-10-05 17:01:50 +0530

    design-completed

--- apps/web/lib/components/auth/RecoveryPasswordForm.tsx
+++ apps/web/lib/components/auth/RecoveryPasswordForm.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { MailCheck, Smartphone } from 'lucide-react';
+import Link from 'next/link';
 import { useState } from 'react';
 import { Button, Input } from 'ui';
 
 import { OtpVerifyForm } from './OtpVerifyForm';
-// import { ResetPasswordForm } from './ResetPasswordForm';
+import { ResetPasswordForm } from './ResetPasswordForm';
 
 export function RecoveryPasswordForm() {
-  const [isOtp, setIsOtp] = useState(true);
+  const [isOtp, setIsOtp] = useState('recover');
   const [isEmailInput, setIsEmailInput] = useState(true);
   const [isNumberInput, setIsNumberInput] = useState(false);
   const handleEmailVerificationClick = () => {
@@ -21,76 +22,91 @@ export function RecoveryPasswordForm() {
   };
   const handleSendLinkClick = (e) => {
     e.preventDefault();
-    setIsOtp(false);
+    setIsOtp('otp');
   };
   return (
-    <div>
-      {isOtp && (
-        <form onSubmit={handleSendLinkClick} className="mt-14">
-          <div className="mt-16 flex flex-col">
-            <span className="mt-4 text-2xl font-semibold">
-              Recover Your Account
-            </span>
-            <p className="mt-2 text-sm font-normal text-gray-800">
-              Select option to recover your account with
-              <span className="text-blue-600">acadx</span>
-            </p>
-          </div>
-          <div className="mt-4 flex flex-col sm:gap-4 lg:flex-row">
-            <Button
-              type="button"
-              className={`mt-3 h-20 w-full rounded-xl bg-transparent p-2 text-base text-gray-800 outline  hover:text-white  ${
-                isEmailInput
-                  ? 'text-primary outline-primary-500'
-                  : 'outline-gray-300'
-              }`}
-              onClick={handleEmailVerificationClick}
-            >
-              <div className="flex flex-col items-center  text-center">
-                <MailCheck size={18} className="" />
-                <p className="mt-2 text-sm">Recover via email</p>
-              </div>
-            </Button>
-            <Button
-              type="button"
-              className={`mt-3 h-20 w-full rounded-xl bg-transparent text-base text-gray-800 outline hover:text-white  ${
-                !isEmailInput
-                  ? 'text-primary outline-primary-500'
-                  : 'outline-gray-300'
-              }`}
-              onClick={handleNumberVerificationClick}
-            >
-              <div className="flex flex-col items-center text-center">
-                <Smartphone size={18} className="" />
-                <p className="mt-2 text-sm">Recover via phone</p>
-              </div>
-            </Button>
-          </div>
-          {isEmailInput && (
-            <div className="relative">
-              <Input
-                type="email"
-                className="mt-4 text-sm"
-                placeholder="Enter your email"
-              />
+    <>
+      {isOtp == 'recover' && (
+        <>
+          <form onSubmit={handleSendLinkClick} className="mt-14">
+            <div className="mt-16 flex flex-col">
+              <span className="mt-4 text-2xl font-semibold">
+                Recover Your Account
+              </span>
+              <p className="mt-2 text-sm font-normal text-gray-800">
+                Select option to recover your account with
+                <span className="text-blue-600">acadx</span>
+              </p>
             </div>
-          )}
-          {isNumberInput && (
-            <div className="relative">
-              <Input
-                type="text"
-                className="mt-4 text-sm"
-                placeholder="Enter your number"
-              />
+            <div className="mt-4 flex flex-col sm:gap-4 lg:flex-row">
+              <Button
+                type="button"
+                className={`mt-3 h-20 w-full rounded-xl bg-transparent p-2 text-base text-gray-800 outline  hover:text-white  ${
+                  isEmailInput
+                    ? 'text-primary outline-primary-500'
+                    : 'outline-gray-300'
+                }`}
+                onClick={handleEmailVerificationClick}
+              >
+                <div className="flex flex-col items-center  text-center">
+                  <MailCheck size={18} className="" />
+                  <p className="mt-2 text-sm">Recover via email</p>
+                </div>
+              </Button>
+              <Button
+                type="button"
+                className={`mt-3 h-20 w-full rounded-xl bg-transparent text-base text-gray-800 outline hover:text-white  ${
+                  !isEmailInput
+                    ? 'text-primary outline-primary-500'
+                    : 'outline-gray-300'
+                }`}
+                onClick={handleNumberVerificationClick}
+              >
+                <div className="flex flex-col items-center text-center">
+                  <Smartphone size={18} className="" />
+                  <p className="mt-2 text-sm">Recover via phone</p>
+                </div>
+              </Button>
             </div>
-          )}
-          <Button type="submit" className="mt-8 w-full text-white">
-            {' '}
-            Send link
-          </Button>
-        </form>
+            {isEmailInput && (
+              <div className="relative">
+                <Input
+                  type="email"
+                  className="mt-4 text-sm"
+                  placeholder="Enter your email"
+                />
+              </div>
+            )}
+            {isNumberInput && (
+              <div className="relative">
+                <Input
+                  type="text"
+                  className="mt-4 text-sm"
+                  placeholder="Enter your number"
+                />
+              </div>
+            )}
+            <Button type="submit" className="mt-8 w-full text-white">
+              {' '}
+              Send link
+            </Button>
+          </form>
+          <p className=" mt-16 text-center text-sm font-semibold text-gray-800">
+            Don&apos;t receive the link?
+          </p>
+          <Link
+            href="/signin"
+            className=" mt-6 w-full rounded-md border-2 border-gray-300 bg-transparent p-2 text-center text-sm font-semibold text-primary hover:bg-primary hover:text-white"
+          >
+            Resend
+          </Link>
+          <footer className="mt-16 text-center text-sm font-normal text-gray-700">
+            &copy; acadx 2023
+          </footer>
+        </>
       )}
-      {!isOtp && <OtpVerifyForm />}
-    </div>
+      {isOtp == 'otp' && <OtpVerifyForm setIsOtp={setIsOtp} />}
+      {isOtp == 'reset' && <ResetPasswordForm />}
+    </>
   );
 }
--- apps/web/lib/components/auth/ResetPasswordForm.tsx
+++ apps/web/lib/components/auth/ResetPasswordForm.tsx
@@ -1,91 +1,69 @@
 'use client';
 
 import { Eye, EyeOff } from 'lucide-react';
-import Image from 'next/image';
 import { useState } from 'react';
 import { Button, Input } from 'ui';
-
-import logo from '../../../public/assets/images/acadx-logo.png';
 
 export function ResetPasswordForm() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   return (
-    <section className="flex h-full flex-col justify-center sm:flex-row">
-      <section
-        className="hidden flex-grow bg-cover bg-left bg-no-repeat sm:flex sm:w-1/4"
-        style={{
-          backgroundImage: 'url(/assets/images/forgot-password-banner-1.png)',
-          opacity: 0.2,
-        }}
-      ></section>
-      <section className="mt-2 flex h-full translate-y-0 transform flex-col justify-between overflow-auto px-20 transition-all duration-500 ease-in-out sm:w-full md:w-4/5 lg:w-3/5 xl:w-2/5">
-        <div className="flex justify-center">
-          <Image src={logo} className="pt-10 " alt={'logo'} width={100}></Image>
+    <>
+      <form className="mt-16">
+        <div className="mt-16 flex flex-col">
+          <span className="mt-8 text-2xl font-semibold">
+            Reset Your Password
+          </span>
+          <p className="mt-2 text-sm font-normal text-gray-800">
+            Please enter your new password{' '}
+          </p>
         </div>
-        <form className="mt-16">
-          <div className="mt-16 flex flex-col">
-            <span className="mt-4 text-2xl font-semibold">
-              Reset Your Password
-            </span>
-            <p className="mt-2 text-sm font-normal text-gray-800">
-              Please enter your new password{' '}
-            </p>
-          </div>
-          <label className="mt-6 block text-sm font-medium text-slate-500">
-            Password
-          </label>
-          <div className="relative">
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              className="mt-2 text-sm"
-              placeholder="Enter your password"
-            />
-            <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-2 ">
-              <div onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? (
-                  <Eye size={20} strokeWidth={0.5} />
-                ) : (
-                  <EyeOff size={20} strokeWidth={0.5} />
-                )}
-              </div>
+        <label className="mt-6 block text-sm font-medium text-slate-500">
+          Password
+        </label>
+        <div className="relative">
+          <Input
+            type={showPassword ? 'text' : 'password'}
+            className="mt-4 text-sm"
+            placeholder="Enter your password"
+          />
+          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-2 ">
+            <div onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? (
+                <Eye size={20} strokeWidth={0.5} />
+              ) : (
+                <EyeOff size={20} strokeWidth={0.5} />
+              )}
             </div>
           </div>
-          <label className="mt-4 block text-sm font-medium text-slate-500">
-            Confirm Password
-          </label>
-          <div className="relative">
-            <Input
-              type={showConfirmPassword ? 'text' : 'password'}
-              className="mt-2 text-sm"
-              placeholder="Enter your password"
-            />
-            <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-2 ">
-              <div onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
-                {showConfirmPassword ? (
-                  <Eye size={20} strokeWidth={0.5} />
-                ) : (
-                  <EyeOff size={20} strokeWidth={0.5} />
-                )}
-              </div>
+        </div>
+        <label className="mt-6 block text-sm font-medium text-slate-500">
+          Confirm Password
+        </label>
+        <div className="relative">
+          <Input
+            type={showConfirmPassword ? 'text' : 'password'}
+            className="mt-4 text-sm"
+            placeholder="Enter your password"
+          />
+          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-2 ">
+            <div onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+              {showConfirmPassword ? (
+                <Eye size={20} strokeWidth={0.5} />
+              ) : (
+                <EyeOff size={20} strokeWidth={0.5} />
+              )}
             </div>
           </div>
-          <Button type="submit" className="mt-10 w-full text-white">
-            {' '}
-            Update
-          </Button>
-        </form>
-        <footer className="mt-16 text-center text-sm font-normal text-gray-700">
-          <div className="mt-14">&copy; acadx 2023</div>
-        </footer>
-      </section>
-      <section
-        className="hidden flex-grow bg-cover bg-left bg-no-repeat sm:flex sm:w-1/4"
-        style={{
-          backgroundImage: 'url(/assets/images/forgot-password-banner-2.png)',
-          opacity: 0.4,
-        }}
-      ></section>
-    </section>
+        </div>
+        <Button type="submit" className="mt-14 w-full text-white">
+          {' '}
+          Update
+        </Button>
+      </form>
+      <footer className="mt-16 text-center text-sm font-normal text-gray-700">
+        <div className="mt-16">&copy; acadx 2023</div>
+      </footer>
+    </>
   );
 }
