21ec33916dab205cfb639a7af2a4e66038e8d7fe
Arasu <malarasuk@gmail.com>
2023-10-11 20:36:15 +0530

    resposive-design-signup

--- apps/web/app/(auth)/signup/page.tsx
+++ apps/web/app/(auth)/signup/page.tsx
@@ -29,30 +29,36 @@ export default async function Page() {
           backgroundImage: 'url(/assets/images/signin-banner.png)',
         }}
       ></section>
-      <section className=" flex h-full overflow-x-hidden px-[3rem] transition-all duration-500 ease-in-out sm:w-2/3 md:w-1/2 md:justify-center lg:w-2/5 xl:w-1/3">
-        <div className="   translate-y-0 flex-col ">
+      <section
+        className=" flex
+ h-screen  translate-y-0 transform flex-col  overflow-x-hidden px-20 opacity-100 transition-all 
+ duration-500 ease-in-out sm:w-2/3 md:w-1/2 md:justify-between lg:w-1/2 xl:w-1/3 xl:justify-between  2xl:justify-center"
+      >
+        <div className=" ">
           <Image src={logo} className="pt-10" alt={'logo'} width={100}></Image>
-          <Text variant="xl-semibold" className="mt-14">
-            Create Your Account
-          </Text>
-          <Text variant="sm-regular" className="text-gray-800">
-            Join acadx and embark on a seamless academic experience.
-          </Text>
+          <div className="mt-2">
+            <Text variant="xl-semibold" className="mt-14">
+              Create Your Account
+            </Text>
+            <Text variant="sm-regular" className="text-gray-800">
+              Join acadx and embark on a seamless academic experience.
+            </Text>
 
-          <SignUpForm />
+            <SignUpForm />
 
-          <Text
-            variant="sm-semibold"
-            className="mt-12 text-center text-gray-800"
-          >
-            Have an account?
-          </Text>
-          <Link
-            href="/signin"
-            className="mt-3 h-[48px] w-full rounded-md border-2 border-gray-300 bg-transparent px-[14px] py-[8px] text-center text-sm font-semibold text-primary hover:bg-primary hover:text-white"
-          >
-            Sign In
-          </Link>
+            <Text
+              variant="sm-semibold"
+              className="mt-12 text-center text-gray-800"
+            >
+              Have an account?
+            </Text>
+            <Link
+              href="/signin"
+              className="mt-3 flex w-full justify-center rounded-md border-2 border-gray-300 bg-transparent p-2 text-sm font-semibold text-primary hover:bg-primary hover:text-white"
+            >
+              Sign In
+            </Link>
+          </div>
           <Footer />
         </div>
       </section>
