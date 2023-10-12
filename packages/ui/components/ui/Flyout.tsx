a5e6fd2bc2f314966122366965dd16fc35076eea
PrabhuSaravanan <prabhu.saravanan@icloud.com>
2023-10-12 15:01:49 +0530

    ...Width Pre-Defined

--- packages/ui/components/ui/Flyout.tsx
+++ packages/ui/components/ui/Flyout.tsx
@@ -6,17 +6,22 @@ import { X } from 'lucide-react';
 interface FlyoutProps {
   isOpen?: boolean;
   onClose?: () => void;
-  widthClass?: string;
   children?: ReactNode; 
+  size?: 'sm' | 'lg';
 }
 
-const Flyout: FC<FlyoutProps> = ({ isOpen, onClose, children, widthClass }) => {
+const sizeClass =  {
+  sm: 'w-64',
+  lg: 'w-100'
+}
+
+const Flyout: FC<FlyoutProps> = ({ isOpen, onClose, children, size='sm'}) => {
 
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 backdrop-filter backdrop-blur-sm bg-black bg-opacity-20">
-          <div className={`fixed bg-white p-3 rounded-l-md top-0 right-0 h-full transform translate-x-0 transition-transform duration-300 ease-in-out ${widthClass}`}>
+          <div className={`fixed bg-white p-3 rounded-l-md top-0 right-0 h-full ${sizeClass[size]}`}>
             {children}
             <button className="absolute bg-black rounded-full text-white top-1 -left-8 p-[0.4em] flex items-center justify-center opacity-90" onClick={onClose}><X size={14} /></button>
           </div>
