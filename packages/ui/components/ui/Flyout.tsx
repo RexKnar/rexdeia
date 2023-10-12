9190c2c3e7b8a7d01bd8958dfbfbc7a5c6d9536b
PrabhuSaravanan <prabhu.saravanan@icloud.com>
2023-10-12 20:49:11 +0530

    ...Flyout Width Size Set & Transition Done

--- packages/ui/components/ui/Flyout.tsx
+++ packages/ui/components/ui/Flyout.tsx
@@ -6,27 +6,41 @@ import { X } from 'lucide-react';
 interface FlyoutProps {
   isOpen?: boolean;
   onClose?: () => void;
-  children?: ReactNode; 
-  size?: 'sm' | 'lg';
+  children?: ReactNode;
+  size?: 'sm' | 'md' | 'lg' | 'xl';
 }
 
-const sizeClass =  {
-  sm: 'w-64',
-  lg: 'w-100'
-}
+const sizeClass = {
+  sm: 'w-1/5',
+  md: 'w-2/5',
+  lg: 'w-3/5',
+  xl: 'w-4/5'
+  
+};
 
-const Flyout: FC<FlyoutProps> = ({ isOpen, onClose, children, size='sm'}) => {
+const Flyout: FC<FlyoutProps> = ({ isOpen, onClose, children, size = 'sm' }) => {
 
   return (
     <>
-      {isOpen && (
-        <div className="fixed inset-0 backdrop-filter backdrop-blur-sm bg-black bg-opacity-20">
-          <div className={`fixed bg-white p-3 rounded-l-md top-0 right-0 h-full ${sizeClass[size]}`}>
-            {children}
-            <button className="absolute bg-black rounded-full text-white top-1 -left-8 p-[0.4em] flex items-center justify-center opacity-90" onClick={onClose}><X size={14} /></button>
-          </div>
+      <div
+        className={`fixed inset-0 backdrop-filter backdrop-blur-sm bg-black bg-opacity-20 transition-opacity duration-300 ${
+          isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+        }`}
+      >
+        <div
+          className={`fixed bg-white p-3 rounded-l-md top-0 right-0 h-full ${sizeClass[size]} transition-transform duration-300 transform ${
+            isOpen ? 'translate-x-0' : 'translate-x-full'
+          }`}
+        >
+          {children}
+          <button
+            className="absolute bg-black rounded-full text-white top-1 -left-8 p-[0.4em] flex items-center justify-center opacity-90"
+            onClick={onClose}
+          >
+            <X size={14} />
+          </button>
         </div>
-      )}
+      </div>
     </>
   );
 };
