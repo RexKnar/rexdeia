f7f2c8e3a45f44975e72ce1b811ba136bf060589
PrabhuSaravanan <prabhu.saravanan@icloud.com>
2023-10-13 14:42:56 +0530

    ...Modal Width Set | 'sm' 'md' 'lg'

--- apps/web/lib/components/admission/ShareFlyout.tsx
+++ apps/web/lib/components/admission/ShareFlyout.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { useState } from 'react';
 import {
   Button,
-  Flyout,
   Sheet,
   SheetContent,
   SheetDescription,
@@ -13,59 +11,25 @@ import {
 } from 'ui';
 
 export function ShareFlyout() {
-  const SHEET_SIDES = ['top', 'right', 'bottom', 'left'] as const;
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
+  
   return (
-    <>
-      <Flyout isOpen={isModalOpen} onClose={closeModal} size="md">
-        <div className="text-center">
-          <h2 className="border bg-slate-400 p-2 text-white">
-            Testing HTML Content
-          </h2>
-          <p className="mt-6 border bg-red-300 p-2 shadow-md">
-            Just testing with some random data!
-          </p>
-          <p className="border bg-red-300 p-2 shadow-md">
-            Just testing with some random data!
-          </p>
-          <p className="border bg-red-300 p-2 shadow-md">
-            Just testing with some random data!
-          </p>
-        </div>
-      </Flyout>
-      <Button className="mt-6 text-white" onClick={openModal}>
-        Open Modal
-      </Button>
-
-      <div className="grid grid-cols-2 gap-2">
-        {SHEET_SIDES.map((side) => (
-          <Sheet key={side}>
-            <SheetTrigger asChild>
-              <Button variant="outline">{side}</Button>
-            </SheetTrigger>
-            <SheetContent side={side} className="bg-white">
-              <SheetHeader>
-                <SheetTitle>Edit profile</SheetTitle>
-                <SheetDescription>
-                  Make changes to your profile here.
-                </SheetDescription>
-              </SheetHeader>
-              <div className="py-4">
-                <div className="py-4">Hello World!</div>
-              </div>
-            </SheetContent>
-          </Sheet>
-        ))}
-      </div>
+    <> 
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" className="bg-blue-500 text-white mt-3 border-2">Open Modal</Button>
+          </SheetTrigger>
+          <SheetContent side="right" widthSize="sm" className="bg-white">
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="py-4">
+              <div className="py-4">Hello World!</div>
+            </div>
+          </SheetContent>
+        </Sheet>
     </>
   );
 }
