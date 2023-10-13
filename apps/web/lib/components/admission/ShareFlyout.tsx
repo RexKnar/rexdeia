8b31da31fa7f111a2bb7f8d646a05b32b1682417
PrabhuSaravanan <prabhu.saravanan@icloud.com>
2023-10-13 14:44:34 +0530

    ...PNPM Run Format

--- apps/web/lib/components/admission/ShareFlyout.tsx
+++ apps/web/lib/components/admission/ShareFlyout.tsx
@@ -11,25 +11,29 @@ import {
 } from 'ui';
 
 export function ShareFlyout() {
-  
   return (
-    <> 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" className="bg-blue-500 text-white mt-3 border-2">Open Modal</Button>
-          </SheetTrigger>
-          <SheetContent side="right" widthSize="sm" className="bg-white">
-            <SheetHeader>
-              <SheetTitle>Edit profile</SheetTitle>
-              <SheetDescription>
-                Make changes to your profile here.
-              </SheetDescription>
-            </SheetHeader>
-            <div className="py-4">
-              <div className="py-4">Hello World!</div>
-            </div>
-          </SheetContent>
-        </Sheet>
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            className="mt-3 border-2 bg-blue-500 text-white"
+          >
+            Open Modal
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="right" widthSize="sm" className="bg-white">
+          <SheetHeader>
+            <SheetTitle>Edit profile</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="py-4">
+            <div className="py-4">Hello World!</div>
+          </div>
+        </SheetContent>
+      </Sheet>
     </>
   );
 }
