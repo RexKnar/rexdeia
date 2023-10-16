3efec55ce3a7669a706c40f29da074dc0f246442
arunselvakumar <b.arunselvakumar@gmail.com>
2023-10-16 09:06:16 +0530

    Refactored 'ShareFlyout' component for better UI structure
    The 'ShareFlyout' component in the admission app has been refactored, involved ordering import statements and reordering several components within the rendering function. The 'useState' hook and an icon component from 'lucide-react' have been added at the top, and the structure of the components has been rearranged to improve readability and consistency.
    
    The goal of this change is to make the structure of the component more intuitive for other developers, contributing to the overall maintainability of the code. The useState hook and the additional icon component will enhance the functionality of the application.

--- apps/web/lib/components/admission/ShareFlyout.tsx
+++ apps/web/lib/components/admission/ShareFlyout.tsx
@@ -1,19 +1,22 @@
 'use client';
 
+import { CalendarIcon } from 'lucide-react';
+import { useState } from 'react';
 import {
   Button,
   Calendar,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
   Switch,
 } from 'ui';
+import { cn } from 'utils';
 
 export function ShareFlyout() {
   const [date, setDate] = useState<Date>();
@@ -35,36 +38,37 @@ export function ShareFlyout() {
               Make changes to your profile here.
             </SheetDescription>
           </SheetHeader>
-          <div className="py-4">
-            <div className="flex items-center space-x-2">
-              <Switch id="test" />
-              <label htmlFor="test">Toggle Switch</label>
-            </div>
-
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant={'outline'}
-                  className={cn(
-                    'w-[240px] justify-start text-left font-normal',
-                    !date && 'text-muted-foreground'
-                  )}
-                >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  Pick a date
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
         </SheetContent>
+
+        <div className="py-4">
+          <div className="flex items-center space-x-2">
+            <Switch id="test" />
+            <label htmlFor="test">Toggle Switch</label>
+          </div>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={'outline'}
+                className={cn(
+                  'w-[240px] justify-start text-left font-normal',
+                  !date && 'text-muted-foreground'
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                Pick a date
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
       </Sheet>
     </>
   );
