b6cd2957fdc44cd20237e98d19ebaa00940e4f0d
Shylendri S <swtshyl36@gmail.com>
2023-10-31 21:16:12 +0530

    Added Get Organization By Id API Added Dynamic College Name, User Full Name

--- apps/web/lib/components/sidebar/SidebarFooter.tsx
+++ apps/web/lib/components/sidebar/SidebarFooter.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import { Avatar, AvatarImage } from 'ui';
 
 import { UserMenu } from '../footer/UserMenu';
+import { useSession } from 'next-auth/react';
 
 export function SidebarFooter() {
+  const session = useSession();
+  if (session.status === 'loading') {
+    return null;
+  }
   return (
     <div className="mb-2 flex items-center justify-between gap-3 px-4 text-lg font-semibold tracking-tight">
       <div className="flex w-[140px] text-left">
@@ -10,8 +17,8 @@ export function SidebarFooter() {
           <AvatarImage src="https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg" />
         </Avatar>
         <div className="ml-2">
-          <h1 className="text-sm font-semibold">John doe</h1>
-          <h2 className="text-sm font-normal text-gray-700">Role</h2>
+          <h1 className="text-sm font-semibold">{session.data.user.name}</h1>
+          <div className="text-sm font-normal text-gray-700">Admin</div>
         </div>
       </div>
       <div>
