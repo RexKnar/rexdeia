fae79d4d0ab1c506d80967a90319f83d60138f43
rakeshmirtha <151906486+rakeshmirtha@users.noreply.github.com>
2023-12-07 04:47:41 +0530

    Sidebar header dropdown fixed (#289)

--- apps/web/lib/components/sidebar/SidebarHeader.tsx
+++ apps/web/lib/components/sidebar/SidebarHeader.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { ChevronDown, Settings } from 'lucide-react';
+import { ChevronDown, Plus, Settings } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from 'ui';
 
 import { useGetUserDetailsQuery } from '../../queries/useGetUserDetailsQuery';
 import { UserMenu } from '../header/UserMenu';
@@ -12,14 +20,29 @@ export function SidebarHeader() {
     <div className="mb-5 flex items-center justify-between gap-3 px-4 text-lg font-semibold tracking-tight">
       <div className="flex items-center gap-4 text-left">
         <UserMenu />
-        <div>
-          <div className="flex cursor-pointer text-sm font-semibold">
-            {isLoading
-              ? 'Loading...'
-              : data.userOrganizations[0].organization.name}
-            <ChevronDown className="ml-2 h-4 w-4 rounded text-gray-800 hover:bg-gray-200" />
-          </div>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <div className="flex cursor-pointer text-sm font-semibold">
+              {isLoading
+                ? 'Loading...'
+                : data.userOrganizations[0].organization.name}
+              <ChevronDown className="ml-2 h-4 w-4 rounded text-gray-800 hover:bg-gray-200" />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="ml-4 flex w-52 flex-col rounded-md border border-primary-100 bg-white py-4 shadow-md"
+            align="end"
+            sideOffset={15}
+          >
+            <DropdownMenuGroup className="w-screen">
+              <DropdownMenuSeparator className="bg-gray-100 text-gray-500" />
+              <DropdownMenuItem className="flex cursor-pointer items-center space-x-2 p-4 text-sm">
+                <Plus className="h-5 w-5 rounded bg-primary-200 text-primary" />
+                <div className="text-primary">Add organisation</div>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <div className="p-2 ">
         <Settings className="cursor-pointer text-gray-700" />
