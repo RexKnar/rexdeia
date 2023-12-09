4a73a5f498542a49def067470210110011e2f622
gopikumarpgk <gopikukmarcs443@gmail.com>
2023-12-09 13:54:11 +0530

    review comments addressed

--- apps/web/app/(protected)/academics/regulation/page.tsx
+++ apps/web/app/(protected)/academics/regulation/page.tsx
@@ -4,7 +4,6 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../../../../lib/auth';
 import { PageHeader } from '../../../../lib/components/PageHeader';
 import { PathBreadcrumb } from '../../../../lib/components/PathBreadcrumb';
-import { RegulationFilterOptions } from './components/RegulationFilterOptions';
 import { RegulationListTable } from './components/RegulationListTable';
 import { RegulationShareFlyout } from './components/RegulationShareFlyout';
 
@@ -27,7 +26,6 @@ export default async function Page() {
       </div>
 
       <div className="mt-4 rounded-md bg-white p-3">
-        <RegulationFilterOptions></RegulationFilterOptions>
         <RegulationListTable></RegulationListTable>
       </div>
     </section>
