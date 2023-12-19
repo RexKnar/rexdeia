e276b92be4c5466df3cced408cbed950e95ff862
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2023-12-19 08:42:05 +0530

    fix: Moved trigger out of the flyout.

---
+++ apps/web/app/(protected)/academics/regulation/components/RegulationsPageHeader.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { PageTitle } from '../../../../../lib/components/PageTitle';
+import { Button } from 'ui';
+import React from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+export function RegulationsPageHeader() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const openFlyout = () => {
+    const params = new URLSearchParams(searchParams);
+    params.set('isFlyoutOpen', 'true');
+
+    router.push(pathname + '?' + params.toString());
+  };
+
+  return (
+    <section className="flex justify-between px-2">
+      <PageTitle title="Regulations" />
+      <Button variant="default" onClick={() => openFlyout()}>
+        Add Regulation
+      </Button>
+    </section>
+  );
+}
