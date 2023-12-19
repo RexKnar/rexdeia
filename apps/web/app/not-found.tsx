4aba237f27f519cb82b546d7b31357cb74a26ea3
rakeshmirtha <151906486+rakeshmirtha@users.noreply.github.com>
2023-12-19 15:35:14 +0530

    404erorpage (#334)
    * Error page added
    
    * Error page created
    
    * errorpage spacing fixed

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,3 +1,32 @@
+import 'configs/tailwind/styles.css';
+
+import Image from 'next/image';
+import { Text } from 'ui';
+
+import { LinkButton } from '../lib/components/LinkButton';
+import image from '../public/assets/images/ErrorImage.gif';
+
 export default async function NotFoundPage() {
-  return <h1>This is 404 Page</h1>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center py-64">
+      <Image
+        src={image}
+        alt="error-404"
+        aria-label="error-404"
+        className="pointer-events-none  mb-px w-96"
+      />
+
+      <Text variant="3xl-bold" className="mb-4 text-black">
+        Something’s missing.
+      </Text>
+      <Text variant="sm-semibold" className="mb-4 text-gray-700">
+        The page you looking for is not found.
+      </Text>
+      <div>
+        <LinkButton variant="primary" url="/admission/dashboard">
+          Go to dashboard
+        </LinkButton>
+      </div>
+    </div>
+  );
 }
