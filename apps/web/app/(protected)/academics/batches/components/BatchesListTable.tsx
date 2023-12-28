9a424fd1af070e1221a99905583afe4ad0149259
sobinesh <sobenesh4@gmail.com>
2023-12-28 17:11:52 +0530

    batch-layout-loader

--- apps/web/app/(protected)/academics/batches/components/BatchesListTable.tsx
+++ apps/web/app/(protected)/academics/batches/components/BatchesListTable.tsx
@@ -217,7 +217,7 @@ export function BatchesListTable() {
                 })}
                 <TableHead>
                   <Button variant="ghost" className="px-0">
-                    Actions
+                    Action
                   </Button>
                 </TableHead>
               </TableRow>
