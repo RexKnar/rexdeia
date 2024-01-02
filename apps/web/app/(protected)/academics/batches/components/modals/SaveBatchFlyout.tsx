b1cb5e7a48637e0022df7fed41d6e26d154915d2
sobinesh <84116767+sobinessh@users.noreply.github.com>
2024-01-02 12:55:49 +0530

    flyout loader added and toogle button edit bug fixed (#398)
    * flyout loader added and toogle button edit bug fixed
    
    * flyout loader added and toogle button bug fixed updated
    
    * updated
    
    ---------
    
    Co-authored-by: sobinesh <sobenesh4@gmail.com>

--- apps/web/app/(protected)/academics/batches/components/modals/SaveBatchFlyout.tsx
+++ apps/web/app/(protected)/academics/batches/components/modals/SaveBatchFlyout.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Loader2, PlusCircle } from 'lucide-react';
 import {
   parseAsBoolean,
@@ -17,6 +16,7 @@ import {
   SheetContent,
   SheetHeader,
   SheetTitle,
+  Spinner,
   Switch,
   Text,
 } from 'ui';
@@ -37,6 +37,7 @@ export function SaveBatchFlyout() {
 
   const [page] = useQueryState('page', parseAsInteger.withDefault(1));
   const [limit] = useQueryState('limit', parseAsInteger.withDefault(10));
+  const [activeToggleFlag, setActiveToggleFlag] = useState(false);
 
   const {
     register,
@@ -56,7 +57,11 @@ export function SaveBatchFlyout() {
     },
   });
 
-  const { data: currentBatch } = useGetBatchByIdQuery(batchId, {
+  const {
+    data: currentBatch,
+    isLoading: isCurrentBatchLoading,
+    isFetching: isCurrentBatchFetching,
+  } = useGetBatchByIdQuery(batchId, {
     enabled: !!batchId,
   });
 
@@ -79,6 +84,7 @@ export function SaveBatchFlyout() {
       setValue('description', description);
       setValue('endYear', new Date(endYear));
       setValue('startYear', new Date(startYear));
+      setActiveToggleFlag(isActive);
 
       setEndYear(new Date(endYear));
       setStartYear(new Date(startYear));
@@ -134,134 +140,144 @@ export function SaveBatchFlyout() {
           className="bg-white p-10"
           onCloseClick={() => closeFlyout()}
         >
-          <form onSubmit={handleSubmit(saveBatch)}>
-            <SheetHeader>
-              <SheetTitle className="mb-5">
-                <div className="sm:grid sm:grid-cols-1 sm:gap-4 md:grid md:grid-cols-1 md:gap-4 lg:flex lg:justify-between">
-                  <div className="flex items-center">
-                    <PlusCircle size={20} strokeWidth={1.5} />
-                    <Text variant="lg-semibold" className="ml-2">
-                      {batchId ? 'Update Batch' : 'Add Batch'}
-                    </Text>
-                  </div>
-                  <div className="flex items-center">
-                    <Switch
-                      id="isActive"
-                      {...register('isActive')}
-                      onCheckedChange={(value) => {
-                        setValue('isActive', value);
-                      }}
-                    />
-                    <label
-                      htmlFor="isActive"
-                      className="ml-2 text-sm font-semibold"
-                    >
-                      {watch('isActive') ? 'Active' : 'Inactive'}
-                    </label>
-                  </div>
-                </div>
-              </SheetTitle>
-              <hr className="border-t border-gray-300"></hr>
-            </SheetHeader>
-
-            <div className="mt-5">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="text-sm font-semibold text-gray-700"
-                >
-                  Batch Name
-                </label>
-                <Input
-                  {...register('name', {
-                    required: 'Batch Name is Required',
-                  })}
-                  className="mt-2"
-                  id="name"
-                />
-                <p
-                  className={`h-2 p-1 text-sm text-red-600 ${
-                    fieldErrors.name
-                      ? 'opacity-100 transition-opacity duration-300'
-                      : 'opacity-0 transition-opacity duration-300'
-                  }`}
-                >
-                  {fieldErrors.name?.message as string}
-                </p>
-              </div>
-              <div>
-                <label
-                  htmlFor="name"
-                  className="text-sm font-semibold text-gray-700"
-                >
-                  Batch Description
-                </label>
-                <Input
-                  {...register('description')}
-                  className="mt-2"
-                  id="name"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="startYear"
-                  className="text-sm font-semibold text-gray-700"
-                >
-                  Start Year
-                </label>
-                <DateSelector
-                  id="startYear"
-                  {...register('startYear')}
-                  onChange={(value) => {
-                    setStartYear(value);
-                    setValue('startYear', value);
-                  }}
-                  showYearPicker
-                  dateFormat="yyyy"
-                  selected={startYear}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="endYear"
-                  className="text-sm font-semibold text-gray-700"
-                >
-                  End Year
-                </label>
-                <DateSelector
-                  id="endYear"
-                  {...register('endYear')}
-                  onChange={(value) => {
-                    setEndYear(value);
-                    setValue('endYear', value);
-                  }}
-                  showYearPicker
-                  dateFormat="yyyy"
-                  selected={endYear}
-                />
-              </div>
-              <div className="mt-10">
-                <Button
-                  size="lg"
-                  variant="default"
-                  disabled={isPendingCreateBatches || isPendingUpdateBatches}
-                  aria-disabled={
-                    isPendingCreateBatches || isPendingUpdateBatches
-                  }
-                  className="mx-auto flex justify-center px-12 py-4"
-                >
-                  {isPendingCreateBatches || isPendingUpdateBatches ? (
-                    <div className="flex items-center justify-center">
-                      <Loader2 className="mr-2 h-6 w-6 animate-spin text-white" />
-                      Saving
+          {isCurrentBatchLoading || isCurrentBatchFetching ? (
+            <section className="flex h-96 w-full flex-col items-center justify-center gap-4">
+              <Spinner />
+              <p>Fetching Data</p>
+            </section>
+          ) : (
+            <form onSubmit={handleSubmit(saveBatch)}>
+              <SheetHeader>
+                <SheetTitle className="mb-5">
+                  <div className="sm:grid sm:grid-cols-1 sm:gap-4 md:grid md:grid-cols-1 md:gap-4 lg:flex lg:justify-between">
+                    <div className="flex items-center">
+                      <PlusCircle size={20} strokeWidth={1.5} />
+                      <Text variant="lg-semibold" className="ml-2">
+                        {batchId ? 'Update Batch' : 'Add Batch'}
+                      </Text>
                     </div>
-                  ) : (
-                    `${batchId ? 'Update' : 'Save'}`
-                  )}
-                </Button>
+                    <div className="flex items-center">
+                      <Switch
+                        id="isActive"
+                        {...register('isActive')}
+                        onCheckedChange={(value) => {
+                          setValue('isActive', value);
+                          setActiveToggleFlag(value);
+                        }}
+                        checked={activeToggleFlag}
+                      />
+                      <label
+                        htmlFor="isActive"
+                        className="ml-2 text-sm font-semibold"
+                      >
+                        {watch('isActive') ? 'Active' : 'Inactive'}
+                      </label>
+                    </div>
+                  </div>
+                </SheetTitle>
+                <hr className="border-t border-gray-300"></hr>
+              </SheetHeader>
+
+              <div className="mt-5">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="text-sm font-semibold text-gray-700"
+                  >
+                    Batch Name
+                  </label>
+                  <Input
+                    {...register('name', {
+                      required: 'Batch Name is Required',
+                    })}
+                    className="mt-2"
+                    id="name"
+                  />
+                  <p
+                    className={`h-2 p-1 text-sm text-red-600 ${
+                      fieldErrors.name
+                        ? 'opacity-100 transition-opacity duration-300'
+                        : 'opacity-0 transition-opacity duration-300'
+                    }`}
+                  >
+                    {fieldErrors.name?.message as string}
+                  </p>
+                </div>
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="text-sm font-semibold text-gray-700"
+                  >
+                    Batch Description
+                  </label>
+                  <Input
+                    {...register('description')}
+                    className="mt-2"
+                    id="name"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="startYear"
+                    className="text-sm font-semibold text-gray-700"
+                  >
+                    Start Year
+                  </label>
+                  <DateSelector
+                    id="startYear"
+                    {...register('startYear')}
+                    onChange={(value) => {
+                      setStartYear(value);
+                      setValue('startYear', value);
+                    }}
+                    showYearPicker
+                    dateFormat="yyyy"
+                    selected={startYear}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="endYear"
+                    className="text-sm font-semibold text-gray-700"
+                  >
+                    End Year
+                  </label>
+                  <DateSelector
+                    id="endYear"
+                    {...register('endYear')}
+                    onChange={(value) => {
+                      setEndYear(value);
+                      setValue('endYear', value);
+                    }}
+                    showYearPicker
+                    dateFormat="yyyy"
+                    selected={endYear}
+                  />
+                </div>
+                <div className="mt-10">
+                  <Button
+                    size="lg"
+                    variant="default"
+                    disabled={isPendingCreateBatches || isPendingUpdateBatches}
+                    aria-disabled={
+                      isPendingCreateBatches || isPendingUpdateBatches
+                    }
+                    className="mx-auto flex justify-center px-12 py-4"
+                  >
+                    {isPendingCreateBatches || isPendingUpdateBatches ? (
+                      <div className="flex items-center justify-center">
+                        <Loader2 className="mr-2 h-6 w-6 animate-spin text-white" />
+                        Saving
+                      </div>
+                    ) : (
+                      `${batchId ? 'Update' : 'Save'}`
+                    )}
+                  </Button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          )}
+          {/* </If> */}
         </SheetContent>
       </Sheet>
     </section>
