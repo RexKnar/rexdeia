6062a933962560edb55586b8b936be5b6dc52a1f
rakeshmirtha <151906486+rakeshmirtha@users.noreply.github.com>
2024-01-05 08:12:54 +0530

    Feature/mirtha/class/UI (#415)
    * Added Class UI
    
    * Added Class UI

--- apps/web/app/(protected)/academics/class/components/ClassListTable.tsx
+++ apps/web/app/(protected)/academics/class/components/ClassListTable.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from '@tanstack/react-table';
-import { GripVertical } from 'lucide-react';
 import { parseAsInteger, useQueryState } from 'next-usequerystate';
+import React from 'react';
 import { When } from 'react-if';
 import {
-  Button,
   Pagination,
   Select,
   SelectContent,
@@ -22,47 +12,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from 'ui';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from 'ui/components/ui/Table';
-import { cn } from 'utils';
 
 import { useGetClassListQuery } from '../../../../../lib/queries/class/useGetClassListQuery';
+import { ClassWidget } from './ClassWidget';
 
 export function ClassList() {
-  const columns: ColumnDef<any>[] = [
-    {
-      accessorKey: 'name',
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-          >
-            Class Name
-          </Button>
-        );
-      },
-    },
-    {
-      accessorKey: 'isActive',
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-          >
-            Active Status
-          </Button>
-        );
-      },
-    },
-  ];
   const [page, setPage] = useQueryState('page', parseAsInteger.withDefault(1));
   const [limit, setLimit] = useQueryState(
     'limit',
@@ -74,81 +28,27 @@ export function ClassList() {
       page,
       limit,
     });
+  const widgetsData = Array.from({ length: 11 }, (_, index) => ({
+    widgetId: index + 1,
+  }));
 
-  const table = useReactTable({
-    columns,
-    data: classList?.data || [],
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-  });
+  // const table = useReactTable({
+  //   columns,
+  //   data: classList?.data || [],
+  //   getCoreRowModel: getCoreRowModel(),
+  //   getSortedRowModel: getSortedRowModel(),
+  //   getFilteredRowModel: getFilteredRowModel(),
+  //   getPaginationRowModel: getPaginationRowModel(),
+  // });
 
   return (
-    <section className="p-3 ">
-      <div className="mt-4 rounded-md border ">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id} className="cursor-pointer">
-                <TableHead className="cursor-pointer ">S.no</TableHead>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-                <TableHead className="cursor-pointer">Actions</TableHead>
-              </TableRow>
-            ))}
-          </TableHeader>
-
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row, index) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                  className={cn(
-                    'cursor-pointer',
-                    index % 2 !== 0 && 'cursor-pointer bg-gray-200'
-                  )}
-                >
-                  <TableCell>{index + 1}</TableCell>
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                  <TableCell>
-                    <Button variant="destructive" className="mr-1 ">
-                      <GripVertical size={16} className="mr-2 text-black" />
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No Classes Found
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+    <section className=" p-3">
+      <div className="flex flex-wrap gap-4">
+        {widgetsData.map((widgetData) => (
+          <ClassWidget key={widgetData.widgetId} />
+        ))}
       </div>
+      <div className="mt-4 rounded-md border "></div>
       <When condition={classList?.data?.length && !isclassListLoading}>
         <section className="mt-5 flex justify-between">
           <div className="justify-left flex w-2/6">
