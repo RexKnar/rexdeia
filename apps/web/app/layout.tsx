65c8a73658d96a16d000fb3033d1893e70f18551
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2024-01-07 13:32:20 +0530

    feat: Added `favicons`.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -21,6 +21,27 @@ const roboto_mono = Roboto_Mono({
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable} ${roboto_mono.variable}`}>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>acadx.io | Elevate your academic experience</title>
+      </head>
       <body className="h-screen bg-gray-50">
         <Providers>{children}</Providers>
       </body>
