2aed0dcda31f726c630dc0d9220bbeae35f5e066
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2024-01-07 13:32:20 +0530

    feat: Added `favicons`.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -23,20 +23,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className={`${inter.variable} ${roboto_mono.variable}`}>
       <head>
         <link
-          rel="apple-touch-icon"
           sizes="180x180"
+          rel="apple-touch-icon"
           href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
-          type="image/png"
           sizes="32x32"
+          type="image/png"
           href="/favicon-32x32.png"
         />
         <link
           rel="icon"
-          type="image/png"
           sizes="16x16"
+          type="image/png"
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
