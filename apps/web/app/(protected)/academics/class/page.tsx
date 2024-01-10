9831a6c5226a066c2aaad0f4cc0c8d3664aac338
rakeshmirtha <151906486+rakeshmirtha@users.noreply.github.com>
2024-01-10 22:21:18 +0530

    ClassList bug fix (#457)

--- apps/web/app/(protected)/academics/class/page.tsx
+++ apps/web/app/(protected)/academics/class/page.tsx
@@ -1,11 +1,9 @@
-import { FileText } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth';
 
 import { authOptions } from '../../../../lib/auth';
-import { LinkButton } from '../../../../lib/components/LinkButton';
-import { PageTitle } from '../../../../lib/components/PageTitle';
 import { ClassList } from './_components/ClassList';
+import { ClassPageHeader } from './_components/ClassPageHeader';
 import SaveSectionFlyout from './[id]/section/[sectionId]/_components/SaveSectionFlyout';
 
 export default async function Page() {
@@ -16,15 +14,11 @@ export default async function Page() {
   }
 
   return (
-    <section className="mx-auto my-5 rounded-md bg-white p-6">
-      <PageTitle title="Class List" className="mb-3" />
-      <div className="mx-7 flex justify-end">
-        <LinkButton variant="primary" url="class/add">
-          <FileText size={16} className="mr-2" />
-          Add Class
-        </LinkButton>
-      </div>
-      <ClassList />
+    <section className="flex flex-col gap-6">
+      <ClassPageHeader />
+      <section className="space-y-2 rounded-md bg-white p-4">
+        <ClassList />
+      </section>
       <SaveSectionFlyout />
     </section>
   );
