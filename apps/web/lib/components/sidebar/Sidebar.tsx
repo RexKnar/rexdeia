6c8370f1842b06094c112604df98e0f54b566610
Ibrahim113502 <130550008+Ibrahim113502@users.noreply.github.com>
2024-01-29 04:54:42 -0800

    Logo Fixed in sidebar (#522)
    * Logo Fixed in sidebar
    
    * Logo Fixed in sidebar
    
    * Logo Fixed in sidebar
    
    * Logo Fixed in sidebar

--- apps/web/lib/components/sidebar/Sidebar.tsx
+++ apps/web/lib/components/sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@
 import { AnimatePresence } from 'framer-motion';
 
 import { menuItems } from './data';
+import { SidebarFooter } from './SidebarFooter';
 import { SidebarHeader } from './SidebarHeader';
 import { SidebarItem } from './SidebarItem';
 
@@ -10,14 +11,19 @@ export function Sidebar() {
   return (
     <AnimatePresence>
       <aside className="fixed flex h-screen w-72 flex-col overflow-y-auto border-r bg-white">
-        <nav>
+        <nav className=" h-full">
           <SidebarHeader />
-          <div className="mt-14 py-3">
-            {menuItems.map((item) => (
-              <div className="px-2 py-1" key={item.id}>
-                <SidebarItem {...item} />
-              </div>
-            ))}
+          <div className="flex h-full flex-col justify-between space-y-4">
+            <div className="mt-14 py-3">
+              {menuItems.map((item) => (
+                <div className="px-2 py-1" key={item.id}>
+                  <SidebarItem {...item} />
+                </div>
+              ))}
+            </div>
+            <div>
+              <SidebarFooter />
+            </div>
           </div>
         </nav>
       </aside>
