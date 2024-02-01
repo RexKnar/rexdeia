9766c402558910673104c24e6034a14cec50eb6f
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2024-02-01 08:56:45 +0530

    chore: Build issue fixed.

--- apps/web/app/api/staff/[id]/route.ts
+++ apps/web/app/api/staff/[id]/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 
 import { authOptions } from '../../../../lib/auth';
-import { deleteStaffById, getStaffById, updateStaffById } from '../service';
 import { validateUpdateStaff } from '../schemas';
+import { deleteStaffById, getStaffById, updateStaffById } from '../service';
 
 export async function GET(_: NextRequest, { params: { id } }) {
   const session = await getServerSession(authOptions);
