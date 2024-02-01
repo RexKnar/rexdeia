ba2cad3d50908a3e46f57f3bd2f9460064471996
Ibrahim113502 <130550008+Ibrahim113502@users.noreply.github.com>
2024-01-31 19:49:15 -0800

    subject type get integration (#570)

--- apps/web/app/(protected)/academics/subjects/subject-type/_components/SubjectTypeListTable.tsx
+++ apps/web/app/(protected)/academics/subjects/subject-type/_components/SubjectTypeListTable.tsx
@@ -1,39 +1,243 @@
 'use client';
 
-import { Pencil, Trash2 } from 'lucide-react';
-import { Button } from 'ui';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { Loader2, Pencil, Trash2 } from 'lucide-react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
+import { When } from 'react-if';
+import {
+  Button,
+  Pagination,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from 'ui';
 import {
   Table,
   TableBody,
   TableCell,
   TableHead,
+  TableHeader,
   TableRow,
 } from 'ui/components/ui/Table';
+import { cn } from 'utils';
+
+import { SubjectTypeModel } from '../../../../../../lib/domain/subject';
+import { useGetSubjectTypeList } from '../../../../../../lib/queries/subject-type/useGetSubjectTypeQuery';
+
+const columns: ColumnDef<SubjectTypeModel>[] = [
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="px-0"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Subject Type Name
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'isActive',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="px-0"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Status
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <span
+          className={cn(
+            'ml-1 rounded px-2 py-1 text-center text-sm font-medium text-gray-100',
+            row.original.isActive ? 'bg-green-600' : 'bg-red-600'
+          )}
+        >
+          {row.original.isActive ? 'Active' : 'Inactive'}
+        </span>
+      );
+    },
+  },
+];
 
 export function SubjectTypeListTable() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const page = parseInt(searchParams.get('page')) || 1;
+  const limit = parseInt(searchParams.get('limit')) || 10;
+
+  const { data: subjectTypeListResponse, isLoading: isSubjectTypeListLoading } =
+    useGetSubjectTypeList({
+      page,
+      limit,
+    });
+
+  const handleOnPageChange = useCallback(
+    (page: number) => {
+      const params = new URLSearchParams(searchParams);
+      params.set('page', page.toString());
+
+      router.push(pathname + '?' + params.toString());
+    },
+    [searchParams, pathname, router]
+  );
+
+  const table = useReactTable({
+    columns,
+    data: subjectTypeListResponse?.data || [],
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
   return (
     <section>
-      <div className="rounded-md ">
+      <div>
         <Table>
-          <TableRow className="cursor-pointer hover:bg-white">
-            <TableHead>Name</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow
+                key={headerGroup.id}
+                className="cursor-pointer hover:bg-white"
+              >
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+                <TableHead>
+                  <Button variant="ghost" className="px-0">
+                    Actions
+                  </Button>
+                </TableHead>
+              </TableRow>
+            ))}
+          </TableHeader>
           <TableBody>
-            <TableCell className="font-medium">Subject Name</TableCell>
-            <TableCell>isActive</TableCell>
-            <TableCell className="flex w-24 justify-between">
-              <Button variant="mild" className="h-auto px-3 py-2">
-                <Pencil size={12} className="text-center text-black" />
-              </Button>
-              <Button variant="mild" className="h-auto px-3 py-2">
-                <Trash2 size={12} className="text-center text-red-600" />
-              </Button>
-            </TableCell>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                  <TableCell className="w-52">
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button
+                          onClick={() => {
+                            const params = new URLSearchParams(searchParams);
+                            params.set('isSubjectTypeFlyoutOpen', 'true');
+                            params.set('subjectType', row.original.id);
+                            router.push(pathname + '?' + params.toString());
+                          }}
+                          className="mr-2 h-auto px-3 py-2"
+                          variant="mild"
+                        >
+                          <Pencil
+                            size={12}
+                            className="text-center text-black"
+                          />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>
+                          <span>Edit</span>
+                        </p>
+                      </TooltipContent>
+                    </Tooltip>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button
+                          className="h-auto px-3 py-2"
+                          variant="mild"
+                          disabled={
+                            row.original.isNewlyAdded || row.original.isUpdating
+                          }
+                        >
+                          {row.original.isDeleting ? (
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin text-red-600" />
+                          ) : (
+                            <Trash2
+                              size={12}
+                              className="text-center text-red-600 "
+                            />
+                          )}
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>
+                          <span>Delete</span>
+                          <span className="mx-1 font-semibold">{`${row.original.name}`}</span>
+                        </p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={5} className="h-24 text-center">
+                  {isSubjectTypeListLoading
+                    ? 'Loading...'
+                    : 'No Subject Type Found'}
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </div>
+      <When
+        condition={
+          subjectTypeListResponse?.data?.length && !isSubjectTypeListLoading
+        }
+      >
+        <Pagination
+          value={limit.toString()}
+          onPageChange={handleOnPageChange}
+          pageSize={subjectTypeListResponse?.limit || 0}
+          totalRecords={subjectTypeListResponse?.total || 0}
+          disabled={isSubjectTypeListLoading}
+          onValueChange={(value) => {
+            const params = new URLSearchParams(searchParams);
+            params.set('limit', value.toString());
+
+            router.push(pathname + '?' + params.toString());
+          }}
+        />
+      </When>
     </section>
   );
 }
