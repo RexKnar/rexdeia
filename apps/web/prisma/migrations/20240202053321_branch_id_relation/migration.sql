f25849ce1038782cf10b3ff1f605a5528430c45a
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2024-02-02 11:07:38 +0530

    feat: Updated subject to be retrieved based on branch id

--- apps/web/prisma/migrations/20240202053321_branch_id_relation/migration.sql
+++ apps/web/prisma/migrations/20240202053321_branch_id_relation/migration.sql
@@ -1,8 +1,5 @@
--- DropForeignKey
-ALTER TABLE "Subject" DROP CONSTRAINT "Subject_branchId_fkey";
-
 -- AlterTable
-ALTER TABLE "Subject" ALTER COLUMN "branchId" DROP NOT NULL;
+ALTER TABLE "Subject" ADD COLUMN     "branchId" UUID;
 
 -- AddForeignKey
 ALTER TABLE "Subject" ADD CONSTRAINT "Subject_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "Branch"("_id") ON DELETE SET NULL ON UPDATE CASCADE;
