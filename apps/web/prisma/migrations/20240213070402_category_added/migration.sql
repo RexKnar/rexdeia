38bd8aa85831fd8f3f49abdec9867e9937be91a4
gopikumarpgk <gopikukmarcs443@gmail.com>
2024-02-13 13:32:22 +0530

    local commit

--- apps/web/prisma/migrations/20240213070402_category_added/migration.sql
+++ apps/web/prisma/migrations/20240213070402_category_added/migration.sql
@@ -2,15 +2,19 @@
 CREATE TABLE "Category" (
     "_id" UUID NOT NULL,
     "name" TEXT NOT NULL,
-    "parent" UUID NOT NULL,
+    "parent" UUID,
     "isActive" BOOLEAN NOT NULL,
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
     "branchId" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("_id")
 );
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_parent_fkey" FOREIGN KEY ("parent") REFERENCES "Category"("_id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Category" ADD CONSTRAINT "Category_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "Branch"("_id") ON DELETE RESTRICT ON UPDATE CASCADE;
