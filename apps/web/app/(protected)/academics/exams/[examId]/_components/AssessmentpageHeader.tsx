ebe2b126f9916078fbc5b124f48e4e863b522eb7
rakeshmirtha <151906486+rakeshmirtha@users.noreply.github.com>
2024-02-28 07:31:11 +0530

    Assessment UI (#675)
    * Assessment UI
    
    * Assessment UI

---
+++ apps/web/app/(protected)/academics/exams/[examId]/_components/AssessmentpageHeader.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { PageTitle } from '../../../../../../lib/components/PageTitle';
+
+export function AssessmentPageHeader() {
+  return (
+    <section className="flex justify-between px-2">
+      <PageTitle title="Assessment" />
+    </section>
+  );
+}
