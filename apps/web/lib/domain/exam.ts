7f03a0a5387a1914e60f338618c0ebee5ee2d5be
gopikumarpgk <gopikukmarcs443@gmail.com>
2024-03-16 20:18:26 +0530

    updated

--- apps/web/lib/domain/exam.ts
+++ apps/web/lib/domain/exam.ts
@@ -1,40 +1,3 @@
-// export type ExamConfigurationModel = {
-//   id: string;
-//   minPassMark: number;
-//   markToConvert: number;
-//   dateToConduct: string;
-//   markToConduct: number;
-// };
-
-// export type CreateExamConfigurationModel = Pick<
-//   ExamConfigurationModel,
-//   'minPassMark' | 'markToConvert' | 'dateToConduct' | 'markToConduct'
-// > & { subjectId: string; classId: string };
-
-// export type ExamModel = {
-//   id: string;
-//   name: string;
-//   termId: string;
-//   classId: string;
-//   batchId: string;
-//   createdAt: string;
-//   updatedAt: string;
-//   sectionId: string;
-//   examTypeId: string;
-//   configuration: ExamConfigurationModel;
-// };
-
-// export type CreateExamModel = Pick<
-//   ExamModel,
-//   | 'name'
-//   | 'termId'
-//   | 'batchId'
-//   | 'classId'
-//   | 'sectionId'
-//   | 'examTypeId'
-//   | 'configuration'
-// > & { subjectId: string };
-
 export type ExamConfigurationModel = {
   name: string;
   assessmentFormatConfiguration: assessmentFormatConfiguration[];
@@ -66,17 +29,16 @@ export type CreateExamConfigurationModel = Pick<
   | 'assessmentFormatConfiguration'
 >;
 
-export type ExamModel = {
+export type ExamTypeModel = {
   id: string;
   name: string;
+  isActive: string;
   termId: string;
-  batchId: string;
-  examTypeId: string;
   createdAt: string;
   updatedAt: string;
 };
 
 export type CreateExamModel = Pick<
-  ExamModel,
-  'name' | 'termId' | 'batchId' | 'examTypeId'
+  ExamTypeModel,
+  'name' | 'termId' | 'isActive'
 >;
