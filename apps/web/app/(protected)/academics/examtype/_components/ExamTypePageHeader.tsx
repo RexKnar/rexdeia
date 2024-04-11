6ab30c3ba26f10388897e058afd9f256dfa8e84a
Shyju S <shyjusha333@gmail.com>
2024-04-11 15:19:17 +0530

    Finished the design for Exam Type Table

---
+++ apps/web/app/(protected)/academics/examtype/_components/ExamTypePageHeader.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+
+import { Button } from 'ui';
+
+import { PageTitle } from '@/components/PageTitle';
+
+
+
+export function ExamTypePageHeader() {
+
+
+  return (
+    <>
+      <section className="flex justify-between px-2">
+        <PageTitle title="Exam Type" />
+        <Button
+          variant="default"
+
+        >
+          Add Exam Type
+        </Button>
+      </section>
+
+    </>
+  );
+}
---
+++ apps/web/app/(protected)/academics/examtype/_components/ExamsTypesTable.tsx
@@ -0,0 +1,70 @@
+
+import React from 'react';
+import { Button } from 'ui';
+import { Table, TableBody, TableCell, TableHeader, TableRow, TableHead } from 'ui/components/ui/Table';
+
+const columns = [
+  {
+    accessorKey: 'name',
+    header: () =>
+      <Button variant="ghost"
+        className="px-0 "
+      >
+        Medium Name
+      </Button>,
+  },
+  {
+    accessorKey: 'additionalColumn',
+    header: () =>
+      <Button variant="ghost"
+        className="px-0 "
+      >
+        Frequency
+      </Button>,
+  },
+  {
+    accessorKey: 'isActive',
+    header: () =>
+      <Button variant="ghost"
+        className="px-0 "
+      >
+        Status
+      </Button>,
+    cell: ({ row }) => (
+      <span className={`ml-1 rounded px-2 py-1 text-center text-sm font-medium text-gray-100
+       ${row.original.isActive ? 'bg-green-600' : 'bg-red-600'}`}>
+        {row.original.isActive ? 'Active' : 'Inactive'}
+      </span>
+    ),
+  },
+];
+
+export function ExamTypeListTable() {
+  return (
+    <section>
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column, index) => (
+                <TableHead className='py-1' key={index}>
+                  {column.header()}
+                </TableHead>
+              ))}
+              <TableHead>
+                <Button variant="ghost" className="px-0">Actions</Button>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan={6} className="h-24 text-center">
+                No Exam Type Found
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  );
+}
