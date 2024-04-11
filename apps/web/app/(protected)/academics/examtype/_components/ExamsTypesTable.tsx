f4f761ec4d5fea7cd01283a39e7bd3f362896b75
Shyju S <shyjusha333@gmail.com>
2024-04-11 18:07:17 +0530

    local commit

--- apps/web/app/(protected)/academics/examtype/_components/ExamsTypesTable.tsx
+++ apps/web/app/(protected)/academics/examtype/_components/ExamsTypesTable.tsx
@@ -1,38 +1,43 @@
-
 import React from 'react';
 import { Button } from 'ui';
-import { Table, TableBody, TableCell, TableHeader, TableRow, TableHead } from 'ui/components/ui/Table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from 'ui/components/ui/Table';
 
 const columns = [
   {
     accessorKey: 'name',
-    header: () =>
-      <Button variant="ghost"
-        className="px-0 "
-      >
+    header: () => (
+      <Button variant="ghost" className="px-0 ">
         Medium Name
-      </Button>,
+      </Button>
+    ),
   },
   {
-    accessorKey: 'additionalColumn',
-    header: () =>
-      <Button variant="ghost"
-        className="px-0 "
-      >
+    accessorKey: 'frequency',
+    header: () => (
+      <Button variant="ghost" className="px-0 ">
         Frequency
-      </Button>,
+      </Button>
+    ),
   },
   {
     accessorKey: 'isActive',
-    header: () =>
-      <Button variant="ghost"
-        className="px-0 "
-      >
+    header: () => (
+      <Button variant="ghost" className="px-0 ">
         Status
-      </Button>,
+      </Button>
+    ),
     cell: ({ row }) => (
-      <span className={`ml-1 rounded px-2 py-1 text-center text-sm font-medium text-gray-100
-       ${row.original.isActive ? 'bg-green-600' : 'bg-red-600'}`}>
+      <span
+        className={`ml-1 rounded px-2 py-1 text-center text-sm font-medium text-gray-100
+       ${row.original.isActive ? 'bg-green-600' : 'bg-red-600'}`}
+      >
         {row.original.isActive ? 'Active' : 'Inactive'}
       </span>
     ),
@@ -42,29 +47,29 @@ const columns = [
 export function ExamTypeListTable() {
   return (
     <section>
-      <div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              {columns.map((column, index) => (
-                <TableHead className='py-1' key={index}>
-                  {column.header()}
-                </TableHead>
-              ))}
-              <TableHead>
-                <Button variant="ghost" className="px-0">Actions</Button>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableHead className="py-1" key={index}>
+                {column.header()}
               </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell colSpan={6} className="h-24 text-center">
-                No Exam Type Found
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+            ))}
+            <TableHead>
+              <Button variant="ghost" className="px-0">
+                Actions
+              </Button>
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell colSpan={6} className="h-24 text-center">
+              No Exam Type Found
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </section>
   );
 }
