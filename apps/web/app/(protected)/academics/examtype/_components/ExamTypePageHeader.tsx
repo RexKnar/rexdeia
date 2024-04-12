0e806514fdf2f14337ccdd00a0925b798557d616
Shyju S <shyjusha333@gmail.com>
2024-04-12 11:39:57 +0530

    Just comitted

--- apps/web/app/(protected)/academics/examtype/_components/ExamTypePageHeader.tsx
+++ apps/web/app/(protected)/academics/examtype/_components/ExamTypePageHeader.tsx
@@ -1,14 +1,36 @@
 'use client';
-
+import dynamic from 'next/dynamic';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Button } from 'ui';
 
 import { PageTitle } from '@/components/PageTitle';
 
+const SaveExamTypeFlyout = dynamic(() =>
+  import('../_modal/SaveExamTypeFlyout').then((mod) => mod.SaveExamTypeFlyout)
+);
+
 export function ExamTypePageHeader() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
-    <section className="flex justify-between px-2">
-      <PageTitle title="Exam Type" />
-      <Button variant="default">Add Exam Type</Button>
-    </section>
+    <>
+      <section className="flex justify-between px-2">
+        <PageTitle title="Exam Type" />
+        <Button
+          variant="default"
+          onClick={async () => {
+            const params = new URLSearchParams(searchParams);
+            params.set('isGroupFlyoutOpen', 'true');
+
+            router.replace(pathname + '?' + params.toString());
+          }}
+        >
+          Add Exam Type
+        </Button>
+      </section>
+      <SaveExamTypeFlyout />
+    </>
   );
 }
---
+++ apps/web/app/(protected)/academics/examtype/_modal/SaveExamTypeFlyout.tsx
@@ -0,0 +1,25 @@
+// import { useForm } from 'react-hook-form';
+import {
+  // Button,
+  // Input,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  // Switch,
+  // Text,
+} from 'ui';
+
+export function SaveExamTypeFlyout() {
+  return (
+    <section>
+      <Sheet>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle></SheetTitle>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+}
