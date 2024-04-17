c69858f5c522d574ba1946b850a828182e3ca76d
Shyju S <shyjusha333@gmail.com>
2024-04-17 17:13:46 +0530

    local commit

--- apps/web/app/(protected)/academics/examtype/_components/ExamsTypesTable.tsx
+++ apps/web/app/(protected)/academics/examtype/_components/ExamsTypesTable.tsx
@@ -1,7 +1,24 @@
 'use client';
+import {
+  // ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import { useGetExamTypeListQuery } from 'lib/queries/exams/useGetExamTypeListQuery';
+import { Loader2, Pencil, Trash2 } from 'lucide-react';
 import React from 'react';
-import { Button } from 'ui';
+import {
+  Button,
+  // Pagination,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  // useToast,
+} from 'ui';
 import {
   Table,
   TableBody,
@@ -16,24 +33,36 @@ import { useQueryParams } from '@/hooks/useQueryParams';
 const columns = [
   {
     accessorKey: 'name',
-    header: () => (
-      <Button variant="ghost" className="px-0 ">
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        className="px-0 "
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
         Exam Type Name
       </Button>
     ),
   },
   {
     accessorKey: 'frequency',
-    header: () => (
-      <Button variant="ghost" className="px-0 ">
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        className="px-0 "
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
         Frequency
       </Button>
     ),
   },
   {
     accessorKey: 'isActive',
-    header: () => (
-      <Button variant="ghost" className="px-0 ">
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        className="px-0 "
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
         Status
       </Button>
     ),
@@ -59,32 +88,129 @@ export function ExamTypeListTable() {
       limit,
     });
   // eslint-disable-next-line no-console
-  console.log(examTypeList, examTypeLoading);
+  const table = useReactTable({
+    columns,
+    data: examTypeList?.data || [],
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
   return (
     <section>
       <Table>
         <TableHeader>
-          <TableRow>
-            {columns.map((column, index) => (
-              <TableHead className="py-1" key={index}>
-                {column.header()}
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow
+              key={headerGroup.id}
+              className="cursor-pointer hover:bg-white"
+            >
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+              <TableHead>
+                <Button variant="ghost" className="px-0">
+                  Actions
+                </Button>
               </TableHead>
-            ))}
-            <TableHead>
-              <Button variant="ghost" className="px-0">
-                Actions
-              </Button>
-            </TableHead>
-          </TableRow>
+            </TableRow>
+          ))}
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell colSpan={6} className="h-24 text-center">
-              No Exam Type Found
-            </TableCell>
-          </TableRow>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+                <TableCell className="w-52">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        onClick={() => {
+                          const params = new URLSearchParams(searchParams);
+                          params.set('isExamTypeFlyoutOpen', 'true');
+                          params.set('examtypeId', row.original.id);
+                          router.push(pathname + '?' + params.toString());
+                        }}
+                        className="mr-2 h-auto px-3 py-2"
+                        variant="mild"
+                      >
+                        <Pencil size={12} className="text-center text-black" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>
+                        <span>Edit</span>
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        className="h-auto px-3 py-2"
+                        variant="mild"
+                        onClick={() => {
+                          setSelectedMedium(row.original);
+                          setShowDeleteConfirmationModal(true);
+                        }}
+                      >
+                        {row.original.isDeleting ? (
+                          <Loader2 className="mr-2 h-4 w-4 animate-spin text-red-600" />
+                        ) : (
+                          <Trash2
+                            size={12}
+                            className="text-center text-red-600 "
+                          />
+                        )}
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>
+                        <span>Delete</span>
+                        <span className="mx-1 font-semibold">{`${row.original.name}`}</span>
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={5} className="h-24 text-center">
+                {isMediumListLoading ? 'Loading...' : 'No Medium Found'}
+              </TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+      {/* <Pagination
+        limit={limit.toString()}
+        onPageChange={handleOnPageChange}
+        pageSize={examTypeList?.limit || 0}
+        totalRecords={examTypeList?.total || 0}
+        disabled={examTypeLoading}
+        onLimitChange={(value) => {
+          const params = new URLSearchParams(searchParams);
+          params.set('limit', value.toString());
+
+          router.push(pathname + '?' + params.toString());
+        }}
+      /> */}
     </section>
   );
 }
