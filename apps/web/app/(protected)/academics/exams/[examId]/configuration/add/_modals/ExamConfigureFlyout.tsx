dfc7c6b3a6012f9beadcf13b143ce70d59fd4135
gopikumarpgk <gopikukmarcs443@gmail.com>
2024-05-14 18:21:54 +0530

    flyout bug

--- apps/web/app/(protected)/academics/exams/[examId]/configuration/add/_modals/ExamConfigureFlyout.tsx
+++ apps/web/app/(protected)/academics/exams/[examId]/configuration/add/_modals/ExamConfigureFlyout.tsx
@@ -95,7 +95,7 @@ export function ExamConfigureFlyout() {
         <SheetContent
           side="right"
           widthSize="sm"
-          className="bg-white p-10"
+          className="p-10 bg-white"
           onCloseClick={() => closeFlyout()}
         >
           <div className="max-h-[95vh] overflow-y-auto">
@@ -113,7 +113,7 @@ export function ExamConfigureFlyout() {
                 </SheetTitle>
                 <hr className="border-t border-gray-300"></hr>
               </SheetHeader>
-              <div className="mt-5 p-1">
+              <div className="p-1 mt-5">
                 <div>
                   <label htmlFor="name" className="text-sm font-semibold">
                     Subject Name
@@ -186,19 +186,19 @@ export function ExamConfigureFlyout() {
                     errorMessage={fieldErrors?.minPassMark?.message.toString()}
                   />
                 </div>
-                <div className="mt-1 flex justify-end">
+                <div className="flex justify-end mt-1">
                   <label className="text-sm font-medium text-gray-600">
                     From conducting mark
                   </label>
                 </div>
               </div>
-              <div className="mt-5 flex flex-wrap">
+              <div className="flex flex-wrap mt-5">
                 {assessmentFormatResponse?.map((assessmentFormat, index) => (
                   <div className="w-1/2" key={assessmentFormat.id}>
                     <Switch
                       id={`assessmentFormatConfiguration.${index}.${assessmentFormat.name}`}
                       key={index}
-                      checked={!!assessmentFormat.id}
+                      checked={assessmentFormat[index]}
                       onCheckedChange={(value) => {
                         value
                           ? append({
@@ -218,7 +218,7 @@ export function ExamConfigureFlyout() {
               </div>
 
               {fields.map((row, index) => (
-                <div key={row.id} className="mt-5 p-1">
+                <div key={row.id} className="p-1 mt-5">
                   <div>
                     <label htmlFor="name" className="text-sm font-semibold">
                       {assessmentFormatResponse[index]?.name}
@@ -299,7 +299,7 @@ export function ExamConfigureFlyout() {
                       errorMessage={fieldErrors?.minPassMark?.message.toString()}
                     />
                   </div>
-                  <div className="mt-1 flex justify-end">
+                  <div className="flex justify-end mt-1">
                     <label className="text-sm font-medium text-gray-600">
                       From conducting mark
                     </label>
@@ -310,7 +310,7 @@ export function ExamConfigureFlyout() {
                 <Button
                   size="lg"
                   variant="default"
-                  className="mx-auto flex justify-center px-12 py-4"
+                  className="flex justify-center px-12 py-4 mx-auto"
                 >
                   Save & Close
                 </Button>
