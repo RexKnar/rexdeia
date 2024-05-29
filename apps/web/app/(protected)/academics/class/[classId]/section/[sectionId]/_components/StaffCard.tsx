90078f9903693a00679577a5988e8a7d72bd9616
gopikumarpgk <gopikukmarcs443@gmail.com>
2024-05-29 17:33:59 +0530

    comments ceared

--- apps/web/app/(protected)/academics/class/[classId]/section/[sectionId]/_components/StaffCard.tsx
+++ apps/web/app/(protected)/academics/class/[classId]/section/[sectionId]/_components/StaffCard.tsx
@@ -21,6 +21,7 @@ type StaffCardProps = {
 };
 
 export function StaffCard(props: StaffCardProps) {
+  const { name, subjects, sectionsHandled } = props;
   return (
     <div className="flex bg-white p-3">
       <div className="w-1/4">
@@ -30,27 +31,25 @@ export function StaffCard(props: StaffCardProps) {
       </div>
 
       <div className="my-auto w-2/4 px-2">
-        <Text variant="base-bold">{props.name}</Text>
-        {props.sectionsHandled.map((section, sectionIndex) => (
+        <Text variant="base-bold">{name}</Text>
+        {sectionsHandled.map((section, sectionIndex) => (
           <div key={sectionIndex} className="inline-flex items-center">
             <span className="ml-1 rounded bg-red-300 p-2 px-1.5 py-0.5 text-sm font-medium text-red-800 dark:bg-red-900 dark:text-red-300">
               {section.name}
             </span>
-            {props.subjects.length > sectionIndex && (
+            {subjects.length > sectionIndex && (
               <div className="ml-2">
                 <Text variant="base-regular">
-                  {props.subjects[sectionIndex].name}
+                  {subjects[sectionIndex].name}
                 </Text>
               </div>
             )}
           </div>
         ))}
-        {props.sectionsHandled.length && (
+        {sectionsHandled.length && (
           <Text variant="base-regular">No Sections</Text>
         )}
-        {props.subjects.length && (
-          <Text variant="base-regular">No Subjects</Text>
-        )}
+        {subjects.length && <Text variant="base-regular">No Subjects</Text>}
       </div>
       <div className="my-auto w-3/4 ">
         <div className="float-end my-auto justify-end p-1">
