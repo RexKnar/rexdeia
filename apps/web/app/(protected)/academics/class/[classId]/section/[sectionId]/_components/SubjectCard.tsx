6ccbb445f016e5d314601991586ad7accc2a6a6b
Sivashunmugam1234 <137296263+Sivashunmugam1234@users.noreply.github.com>
2024-06-05 16:03:25 +0530

    ui completed for assign subject to student

--- apps/web/app/(protected)/academics/class/[classId]/section/[sectionId]/_components/SubjectCard.tsx
+++ apps/web/app/(protected)/academics/class/[classId]/section/[sectionId]/_components/SubjectCard.tsx
@@ -64,6 +64,23 @@ export function SubjectCard({ id, name }: SubjectCardProps) {
                     Remove
                   </Button>
                 </DropdownMenuItem>
+                <DropdownMenuSeparator className="bg-gray-100 text-gray-500" />
+                <DropdownMenuItem className="flex cursor-pointer items-center">
+                  <Button
+                    variant="link"
+                    size="sm"
+                    className="flex-1"
+                    onClick={async () => {
+                      const params = new URLSearchParams(searchParams);
+                      params.set('isAssignSubjectToStudentFlyout', 'true');
+                      params.set('subjectId', id);
+                      params.set('subjectName', name);
+                      router.replace(pathname + '?' + params.toString());
+                    }}
+                  >
+                    Assign elective
+                  </Button>
+                </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
