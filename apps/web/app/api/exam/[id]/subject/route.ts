a625df127e0a807b9cf6446ceb754798dfc1c3d9
Sobinjm <jmsobin@gmail.com>
2024-07-14 21:20:54 +0530

    rank

--- apps/web/app/api/exam/[id]/subject/route.ts
+++ apps/web/app/api/exam/[id]/subject/route.ts
@@ -4,7 +4,10 @@ import { authOptions } from 'lib/auth';
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 
-import { getExamSubjectsByClassSectionId } from './service';
+import {
+  getExamSubjectsByClassSectionId,
+  getExamSubjectsByMaster,
+} from './service';
 
 /**
  * @swagger
@@ -45,6 +48,64 @@ export async function GET(request: NextRequest, { params: { id } }) {
     const sectionId = request.nextUrl.searchParams.get('sectionId');
     const classId = request.nextUrl.searchParams.get('classId');
     const subjectConfigDetail = await getExamSubjectsByClassSectionId(
+      id,
+      classId,
+      sectionId
+    );
+
+    return new NextResponse(JSON.stringify(subjectConfigDetail), {
+      status: StatusCodes.OK,
+    });
+  } catch (e) {
+    captureException(e);
+    return new NextResponse(JSON.stringify({ error: e.message }), {
+      status:
+        e.message === 'VALIDATION_ERROR'
+          ? StatusCodes.BAD_REQUEST
+          : StatusCodes.INTERNAL_SERVER_ERROR,
+    });
+  }
+}
+
+/**
+ * @swagger
+ * /api/exam/{id}/subject/?sectionId=string&classId:
+ *     put:
+ *       summary: Fetch config subjects by examId, classId, sectionId
+ *       description: Fetch configuration details of subject By subjectId
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           required: true
+ *           description: Unique identifier of the subject.
+ *           schema:
+ *             type: string
+ *       responses:
+ *         '200':
+ *           description: Subject configuration is fetched successfully.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 # Define the schema of your class object here
+ *         '400':
+ *           description: Bad request due to validation error.
+ *         '401':
+ *           description: Unauthorized access.
+ *         '500':
+ *           description: Internal server error.
+ */
+export async function PUT(request: NextRequest, { params: { id } }) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new NextResponse(JSON.stringify({ error: 'UNAUTHORIZED' }), {
+      status: StatusCodes.UNAUTHORIZED,
+    });
+  }
+
+  try {
+    const sectionId = request.nextUrl.searchParams.get('sectionId');
+    const classId = request.nextUrl.searchParams.get('classId');
+    const subjectConfigDetail = await getExamSubjectsByMaster(
       id,
       classId,
       sectionId
--- apps/web/app/api/exam/[id]/subject/service.ts
+++ apps/web/app/api/exam/[id]/subject/service.ts
@@ -35,6 +35,8 @@ export async function getExamSubjectsByClassSectionId(
         select: {
           id: true,
           name: true,
+          subjectOrder: true,
+          subjectMaster: true,
         },
       },
       examSubjectPartition: {
@@ -46,4 +48,84 @@ export async function getExamSubjectsByClassSectionId(
   });
 
   return response;
+}
+
+export async function getExamSubjectsByMaster(
+  examId: string,
+  classId?: string,
+  sectionId?: string
+) {
+  const response = await db.examSubject.findMany({
+    where: {
+      examGroup: {
+        examId,
+        classId,
+        sectionId,
+      },
+    },
+    orderBy: {
+      subject: {
+        subjectOrder: 'asc',
+      },
+    },
+    select: {
+      id: true,
+      totalMarks: true,
+      convertTo: true,
+      minMark: true,
+      examGroup: {
+        select: {
+          classId: true,
+          sectionId: true,
+          examId: true,
+          exam: true,
+        },
+      },
+      subject: {
+        select: {
+          id: true,
+          name: true,
+          subjectOrder: true,
+          subjectMaster: true,
+        },
+      },
+      examSubjectPartition: {
+        include: {
+          assessmentFormat: true,
+        },
+      },
+    },
+  });
+
+  // Group subjects by subjectMaster
+  const groupedSubjects = response.reduce(
+    (acc, examSubject) => {
+      const subjectMasterId = examSubject.subject.subjectMaster.id;
+      const subjectMasterName = examSubject.subject.subjectMaster.name;
+
+      if (!acc[subjectMasterId]) {
+        acc[subjectMasterId] = {
+          id: subjectMasterId,
+          name: subjectMasterName,
+          subjects: [],
+        };
+      }
+
+      acc[subjectMasterId].subjects.push({
+        id: examSubject.id,
+        name: examSubject.subject.name,
+        totalMarks: examSubject.totalMarks,
+        convertTo: examSubject.convertTo,
+        minMark: examSubject.minMark,
+        subjectOrder: examSubject.subject.subjectOrder,
+        examSubjectPartition: examSubject.examSubjectPartition,
+      });
+
+      return acc;
+    },
+    {} as Record<string, { id: string; name: string; subjects: any[] }>
+  );
+
+  // Convert the grouped subjects object to an array
+  return Object.values(groupedSubjects);
 }
