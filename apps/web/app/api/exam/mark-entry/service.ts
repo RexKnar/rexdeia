ab953836376dfb55b6adbbd85bc766aa129c06e3
Sobinjm <jmsobin@gmail.com>
2024-09-16 19:32:43 +0530

    markentry fix

--- apps/web/app/api/exam/mark-entry/service.ts
+++ apps/web/app/api/exam/mark-entry/service.ts
@@ -3,39 +3,50 @@ import { EnterMarkEntryModel } from 'lib/domain/mark-entry';
 
 export async function enterMark(markEntryPayload: EnterMarkEntryModel) {
   try {
-    const createdMarkEntries = await db.$transaction(async (prisma) => {
-      const promises = [];
+    const batchSize = 5000; // Adjust based on your needs and database capabilities
+    const createMarks = [];
 
-      for (const entry of markEntryPayload.studentsMarkDetails) {
-        const { studentId, subjects } = entry;
-        for (const studentMark of subjects) {
-          const { marks } = studentMark;
-          for (const mark of marks) {
-            if (mark.mark || mark.attendance) {
-              const where = { id: mark?.id };
-              const data = {
-                studentId: studentId,
-                userId: markEntryPayload.userId,
-                examSubjectId: studentMark.examSubjectId,
-                subjectId: studentMark.subjectId,
-                assessmentFormatId: mark.assessmentFormatId,
-                examSubjectPartitionId: mark.examPartitionId,
-                mark: +mark.mark,
-                attandance: +mark.attendance,
-              };
-              const promise = mark.id
-                ? prisma.mark.update({ where, data })
-                : prisma.mark.create({ data });
-              promises.push(promise);
+    for (const entry of markEntryPayload.studentsMarkDetails) {
+      const { studentId, subjects } = entry;
+      for (const studentMark of subjects) {
+        const { marks } = studentMark;
+        for (const mark of marks) {
+          if (mark.mark || mark.attendance) {
+            const data = {
+              studentId: studentId,
+              userId: markEntryPayload.userId,
+              examSubjectId: studentMark.examSubjectId,
+              subjectId: studentMark.subjectId,
+              assessmentFormatId: mark.assessmentFormatId,
+              examSubjectPartitionId: mark.examPartitionId,
+              mark: +mark.mark,
+              attandance: +mark.attendance,
+            };
+            if (mark.id) {
+              await db.mark.update({
+                where: { id: mark.id },
+                data: data,
+              });
+            } else {
+              createMarks.push(data);
             }
           }
         }
       }
+    }
 
-      return Promise.all(promises);
-    });
+    const createdMarkEntries = [];
 
-    return createdMarkEntries.flat();
+    for (let i = 0; i < createMarks.length; i += batchSize) {
+      const batch = createMarks.slice(i, i + batchSize);
+      const result = await db.mark.createMany({
+        data: batch,
+        skipDuplicates: true,
+      });
+      createdMarkEntries.push(result);
+    }
+
+    return createdMarkEntries;
   } catch (error) {
     console.error('Error creating mark entry:', error);
     throw error;
