63b8e83a6d01671174eec69e9653e856884af538
Sobinjm <jmsobin@gmail.com>
2024-11-05 09:50:22 +0530

    student search and marklist sorting

--- apps/web/lib/components/UserMenu.tsx
+++ apps/web/lib/components/UserMenu.tsx
@@ -1,9 +1,6 @@
 import { ChevronDown, ChevronRight } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 import {
-  Avatar,
-  AvatarImage,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -15,15 +12,11 @@ import {
 import { useGetUserDetailsQuery } from '../queries/useGetUserDetailsQuery';
 
 export function UserMenu() {
-  const router = useRouter();
   const { data, isLoading } = useGetUserDetailsQuery();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center gap-2 outline-none">
-        {/* <Avatar className="h-9 w-9">
-          <AvatarImage src="https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg" />
-        </Avatar> */}
         <Text variant="sm-medium" className="hidden lg:block">
           Hi, {isLoading ? 'Loading...' : data?.name}
         </Text>
