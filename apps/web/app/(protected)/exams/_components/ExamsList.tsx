258d320ddc6abcc212c3ba2accf31e92281cf557
Sobinjm <jmsobin@gmail.com>
2024-12-20 09:33:54 +0530

    overall analytics average mark fix

--- apps/web/app/(protected)/exams/_components/ExamsList.tsx
+++ apps/web/app/(protected)/exams/_components/ExamsList.tsx
@@ -2,7 +2,9 @@
 
 import { useGetExamListQuery } from 'lib/queries/exams/useGetExamListQuery';
 import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
 
+import { DeleteConfirmationModal } from '@/components/modals/DeleteConfirmationModal';
 import { useQueryParams } from '@/hooks/useQueryParams';
 
 import { ExamCard } from './ExamCard';
@@ -19,6 +21,9 @@ export function ExamsList() {
 
   const page = parseInt(getParam('page')) || 1;
   const limit = parseInt(getParam('limit')) || 999;
+  const [showBlockConfirmationModal, setShowBlockConfirmationModal] =
+    useState(false);
+  const [selectedExam, setSelectedExam] = useState<any>(null);
 
   const { data: examListResponse, isLoading: isExamListLoading } =
     useGetExamListQuery({ page, limit });
@@ -34,7 +39,7 @@ export function ExamsList() {
   return (
     <div>
       <h2 className="mb-4 text-sm font-semibold text-gray-600">Exams</h2>
-      <div className="container flex flex-wrap p-4">
+      <div className="container flex flex-wrap gap-4 ">
         {examListResponse?.data?.map((exam, index) => (
           <ExamCard
             key={exam?.id}
@@ -43,11 +48,26 @@ export function ExamsList() {
             termName={exam?.term?.name}
             academicYear={exam?.batch?.name}
             isActive={exam.isActive}
-            className="m-2 w-1/4"
+            isClosed={exam?.blockMarkEntry}
+            className="lg:w-[32.2%] "
             cardColor={cardColors[index % cardColors.length]}
+            setSelectedExam={setSelectedExam}
           />
         ))}
       </div>
+      <DeleteConfirmationModal
+        open={showBlockConfirmationModal}
+        description={`Are you sure you want to ${selectedExam?.id} "${selectedExam?.name}"`}
+        onDeleteClick={async () => {
+          if (selectedExam) {
+            // await deleteBatchAsync(exam?.id);
+            setShowBlockConfirmationModal(false);
+          }
+        }}
+        onCancelClick={() => {
+          setShowBlockConfirmationModal(false);
+        }}
+      />
     </div>
   );
 }
