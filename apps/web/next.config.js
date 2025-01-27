447b0c338ad94b5d9643a4dd8965d44a04214750
Sobinjm <jmsobin@gmail.com>
2025-01-27 07:34:22 +0530

    mark entry bug fix- entry failed if abscent is marked

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -47,16 +47,16 @@ const nextConfig = {
             minChunks: 2,
             reuseExistingChunk: true,
           },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1];
-              return `npm.${packageName.replace('@', '')}`;
-            },
-            chunks: 'all',
-          },
+          // lib: {
+          //   test: /[\\/]node_modules[\\/]/,
+          //   name(module) {
+          //     const packageName = module.context.match(
+          //       /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+          //     )[1];
+          //     return `npm.${packageName.replace('@', '')}`;
+          //   },
+          //   chunks: 'all',
+          // },
         },
       },
     };
