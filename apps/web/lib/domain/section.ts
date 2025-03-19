ab6ea01c311ab6536682633b08f3284c6c204fd4
Rahul K <rahulsachin07314@gmail.com>
2025-03-19 22:19:06 +0530

    subjects List

--- apps/web/lib/domain/section.ts
+++ apps/web/lib/domain/section.ts
@@ -1,11 +1,8 @@
-import { JSX } from 'react';
-
 import { GroupModel } from './group';
 import { MediumModel } from './medium';
 import { CreateSubjectModel, SubjectModel } from './subject';
 
 export type SectionModel = {
-  map(arg0: (section: any) => JSX.Element): import('react').ReactNode;
   id: string;
   name: string;
   isActive: boolean;
