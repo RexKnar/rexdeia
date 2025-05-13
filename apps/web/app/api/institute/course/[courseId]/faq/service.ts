15ede077509540676cf437dc91da37f2eafaa4a5
Rahul K <rahulsachin07314@gmail.com>
2025-05-13 13:00:20 +0530

    craete api for thr course FAQ layout

---
+++ apps/web/app/api/institute/course/[courseId]/faq/service.ts
@@ -0,0 +1,29 @@
+import { db } from 'lib/db';
+
+export const addCourseFAQ = async (courseId, payload) => {
+  const { question, answer } = payload;
+  return await db.instituteCourseFAQ.create({
+    data: {
+      question,
+      answer,
+      instituteCourse: {
+        connect: {
+          id: courseId,
+        },
+      },
+    },
+  });
+};
+
+export const getCourseFAQ = async (courseId: string) => {
+  return await db.instituteCourseFAQ.findMany({
+    where: {
+      instituteCourseId: courseId,
+    },
+    select: {
+      question: true,
+      answer: true,
+      instituteCourseId: true,
+    },
+  });
+};
