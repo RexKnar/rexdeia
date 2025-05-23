db7a149e7467d530ee519762cdfd6c16704e12ad
Sobinjm <jmsobin@gmail.com>
2025-05-23 18:48:20 +0530

    merge fix

---
+++ apps/web/app/api/institute/learner/payments/route.ts
@@ -0,0 +1,33 @@
+import { captureException } from '@sentry/nextjs';
+import { StatusCodes } from 'http-status-codes';
+import { authOptions } from 'lib/auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+
+import { getPaymentListByLearnerId } from './service';
+
+export async function GET(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new NextResponse(JSON.stringify({ error: 'UNAUTHORIZED' }), {
+      status: StatusCodes.UNAUTHORIZED,
+    });
+  }
+
+  try {
+    const { searchParams } = request.nextUrl;
+    const page = parseInt(searchParams.get('page')) || 1;
+    const limit = parseInt(searchParams.get('limit')) || 10;
+
+    const paginatedPaymentList = await getPaymentListByLearnerId(page, limit);
+
+    return new NextResponse(JSON.stringify(paginatedPaymentList), {
+      status: StatusCodes.OK,
+    });
+  } catch (e) {
+    captureException(e);
+    return new NextResponse(e, {
+      status: StatusCodes.BAD_REQUEST,
+    });
+  }
+}
