bbfe14e73d62609ce5e3b4bdc2d91a77420e9705
Kavin Daniel <kavindaniel99@gmail.com>
2025-06-04 17:50:16 +0530

    Create role manageent page

---
+++ apps/web/app/(protected)/academics/role-management/_components/RoleManagementPageHeader.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Button } from 'ui';
+
+import { PageTitle } from '@/components/PageTitle';
+
+import { AddScalesFlyout } from '../_modals/AddScalesFlyout';
+
+const RoleManagementFlyout = dynamic(() =>
+  import('../_modals/SaveRolemanagementFlyout').then((mod) => mod.GradeFlyout)
+);
+
+export function RoleManagementPageHeader() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  return (
+    <>
+      <section className="flex justify-between px-2">
+        <PageTitle title="Role Management" />
+        <Button
+          variant="default"
+          onClick={async () => {
+            const params = new URLSearchParams(searchParams);
+            params.set('isGradeFlyoutOpen', 'true');
+
+            router.replace(pathname + '?' + params.toString());
+          }}
+        >
+          Add Role Management
+        </Button>
+      </section>
+      <RoleManagementFlyout />
+      <AddScalesFlyout />
+    </>
+  );
+}
