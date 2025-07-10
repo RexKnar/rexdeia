c304742182a8015ed010287ed8146720d6de4aa1
Rahul K <rahulsachin07314@gmail.com>
2025-07-10 12:05:07 +0530

    craete and integrate api for assign staff in classLevel ById

--- apps/web/lib/endpoints/index.ts
+++ apps/web/lib/endpoints/index.ts
@@ -183,6 +183,7 @@ export const DELETE_EMPLOYMENT_TYPE_BY_ID = `DELETE_EMPLOYMENT_TYPE_BY_ID`;
 export const GET_SUBJECTS_BY_CLASS_AND_GROUP_ID = `GET_SUBJECTS_BY_CLASS_AND_GROUP_ID`;
 export const ADD_CLASSLEVEL = `ADD_CLASSLEVEL`;
 export const GET_CLASSLEVELS_LIST = `GET_CLASSLEVELS_LIST`;
+export const ASSIGN_STAFF_CLASSLEVEL = `ASSIGN_STAFF_CLASSLEVEL`;
 export const GET_CLASSLEVEL_BY_ID = `GET_CLASSLEVEL_BY_ID`;
 export const GET_CLASSES_BY_CLASSLEVEL_ID = `GET_CLASSES_BY_CLASSLEVEL_ID`;
 export const GET_STUDENTS_BY_CLASSLEVEL_ID = `GET_STUDENTS_BY_CLASSLEVEL_ID`;
@@ -951,6 +952,10 @@ export default <EndpointDetails>{
   [GET_CLASSLEVEL_BY_ID]: {
     requestType: 'GET',
     endpoint: '/api/classLevel/[id]',
+  },
+  [ASSIGN_STAFF_CLASSLEVEL]: {
+    requestType: 'POST',
+    endpoint: '/api/classLevel/[id]/staff',
   },
   [GET_CLASSES_BY_CLASSLEVEL_ID]: {
     requestType: 'GET',
